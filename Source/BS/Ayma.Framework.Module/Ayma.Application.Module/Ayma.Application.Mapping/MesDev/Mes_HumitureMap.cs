40b034c2cb8400791e8c0db58c076cbda9a63a49
周雅波 <1815674+zybanswer@users.noreply.gitee.com>
2019-03-06 15:06:46 +0800

    no commit message

---
+++ Source/BS/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.Mapping/MesDev/Mes_HumitureMap.cs
@@ -0,0 +1,27 @@
+﻿using Ayma.Application.TwoDevelopment.MesDev;
+using System.Data.Entity.ModelConfiguration;
+
+namespace  Ayma.Application.Mapping
+{
+    /// <summary>
+    /// 创 建：超级管理员
+    /// 日 期：2019-03-06 14:49
+    /// 描 述：温湿度采集参数设置
+    /// </summary>
+    public partial class Mes_HumitureMap : EntityTypeConfiguration<Mes_HumitureEntity>
+    {
+        public Mes_HumitureMap()
+        {
+            #region 表、主键
+            //表
+            this.ToTable("MES_HUMITURE");
+            //主键
+            this.HasKey(t => t.ID);
+            #endregion
+
+            #region 配置关系
+            #endregion
+        }
+    }
+}
+
---
+++ Source/BS/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.Mapping/MesDev/Mes_ProceMap.cs
@@ -0,0 +1,27 @@
+﻿using Ayma.Application.TwoDevelopment.MesDev;
+using System.Data.Entity.ModelConfiguration;
+
+namespace  Ayma.Application.Mapping
+{
+    /// <summary>
+    /// 创 建：超级管理员
+    /// 日 期：2019-03-06 14:55
+    /// 描 述：工序管理
+    /// </summary>
+    public partial class Mes_ProceMap : EntityTypeConfiguration<Mes_ProceEntity>
+    {
+        public Mes_ProceMap()
+        {
+            #region 表、主键
+            //表
+            this.ToTable("MES_PROCE");
+            //主键
+            this.HasKey(t => t.P_Record);
+            #endregion
+
+            #region 配置关系
+            #endregion
+        }
+    }
+}
+
