d8240972d02edf9317099c657033e4f6cb805cec
周雅波 <1815674+zybanswer@users.noreply.gitee.com>
2019-03-06 14:40:30 +0800

    no commit message

---
+++ Source/BS/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.TwoDevelopment/MesDev/SalaryManager/Mes_SalaryEntity.cs
@@ -0,0 +1,68 @@
+﻿using Ayma.Util;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ayma.Application.TwoDevelopment.MesDev
+{
+    /// <summary>
+    /// 创 建：超级管理员
+    /// 日 期：2019-03-06 14:22
+    /// 描 述：工资参数设定
+    /// </summary>
+    public partial class Mes_SalaryEntity 
+    {
+        #region 实体成员
+        /// <summary>
+        /// ID
+        /// </summary>
+        [Column("ID")]
+        public string ID { get; set; }
+        /// <summary>
+        /// 用户类型
+        /// </summary>
+        [Column("S_USERKIND")]
+        public string S_UserKind { get; set; }
+        /// <summary>
+        /// 工作类型
+        /// </summary>
+        [Column("S_WORKKIND")]
+        public string S_WorkKind { get; set; }
+        /// <summary>
+        /// 单位时间
+        /// </summary>
+        [Column("S_TIMEUNIT")]
+        public string S_TimeUnit { get; set; }
+        /// <summary>
+        /// 金额
+        /// </summary>
+        [Column("S_PAY")]
+        public decimal? S_Pay { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        [Column("S_REMARK")]
+        public string S_Remark { get; set; }
+        #endregion
+
+        #region 扩展操作
+        /// <summary>
+        /// 新增调用
+        /// </summary>
+        public void Create()
+        {
+            this.ID = Guid.NewGuid().ToString();
+        }
+        /// <summary>
+        /// 编辑调用
+        /// </summary>
+        /// <param name="keyValue"></param>
+        public void Modify(string keyValue)
+        {
+            this.ID = keyValue;
+        }
+        #endregion
+        #region 扩展字段
+        #endregion
+    }
+}
+
---
+++ Source/BS/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.TwoDevelopment/MesDev/SalaryManager/SalaryManagerService.cs
@@ -0,0 +1,157 @@
+﻿using Dapper;
+using Ayma.DataBase.Repository;
+using Ayma.Util;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Ayma.Application.TwoDevelopment.MesDev
+{
+    /// <summary>
+    /// 创 建：超级管理员
+    /// 日 期：2019-03-06 14:22
+    /// 描 述：工资参数设定
+    /// </summary>
+    public partial class SalaryManagerService : RepositoryFactory
+    {
+        #region 获取数据
+
+        /// <summary>
+        /// 获取页面显示列表数据
+        /// </summary>
+        /// <param name="queryJson">查询参数</param>
+        /// <returns></returns>
+        public IEnumerable<Mes_SalaryEntity> GetPageList(Pagination pagination, string queryJson)
+        {
+            try
+            {
+                var strSql = new StringBuilder();
+                strSql.Append("SELECT ");
+                strSql.Append(@"
+                t.ID,
+                t.S_UserKind,
+                t.S_WorkKind,
+                t.S_TimeUnit,
+                t.S_Pay,
+                t.S_Remark
+                ");
+                strSql.Append("  FROM Mes_Salary t ");
+                strSql.Append("  WHERE 1=1 ");
+                var queryParam = queryJson.ToJObject();
+                // 虚拟参数
+                var dp = new DynamicParameters(new { });
+                if (!queryParam["S_UserKind"].IsEmpty())
+                {
+                    dp.Add("S_UserKind",queryParam["S_UserKind"].ToString(), DbType.String);
+                    strSql.Append(" AND t.S_UserKind = @S_UserKind ");
+                }
+                if (!queryParam["S_WorkKind"].IsEmpty())
+                {
+                    dp.Add("S_WorkKind",queryParam["S_WorkKind"].ToString(), DbType.String);
+                    strSql.Append(" AND t.S_WorkKind = @S_WorkKind ");
+                }
+                return this.BaseRepository().FindList<Mes_SalaryEntity>(strSql.ToString(),dp, pagination);
+            }
+            catch (Exception ex)
+            {
+                if (ex is ExceptionEx)
+                {
+                    throw;
+                }
+                else
+                {
+                    throw ExceptionEx.ThrowServiceException(ex);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 获取Mes_Salary表实体数据
+        /// </summary>
+        /// <param name="keyValue">主键</param>
+        /// <returns></returns>
+        public Mes_SalaryEntity GetMes_SalaryEntity(string keyValue)
+        {
+            try
+            {
+                return this.BaseRepository().FindEntity<Mes_SalaryEntity>(keyValue);
+            }
+            catch (Exception ex)
+            {
+                if (ex is ExceptionEx)
+                {
+                    throw;
+                }
+                else
+                {
+                    throw ExceptionEx.ThrowServiceException(ex);
+                }
+            }
+        }
+
+        #endregion
+
+        #region 提交数据
+
+        /// <summary>
+        /// 删除实体数据
+        /// </summary>
+        /// <param name="keyValue">主键</param>
+        /// <returns></returns>
+        public void DeleteEntity(string keyValue)
+        {
+            try
+            {
+                this.BaseRepository().Delete<Mes_SalaryEntity>(t=>t.ID == keyValue);
+            }
+            catch (Exception ex)
+            {
+                if (ex is ExceptionEx)
+                {
+                    throw;
+                }
+                else
+                {
+                    throw ExceptionEx.ThrowServiceException(ex);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 保存实体数据（新增、修改）
+        /// </summary>
+        /// <param name="keyValue">主键</param>
+        /// <returns></returns>
+        public void SaveEntity(string keyValue, Mes_SalaryEntity entity)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(keyValue))
+                {
+                    entity.Modify(keyValue);
+                    this.BaseRepository().Update(entity);
+                }
+                else
+                {
+                    entity.Create();
+                    this.BaseRepository().Insert(entity);
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex is ExceptionEx)
+                {
+                    throw;
+                }
+                else
+                {
+                    throw ExceptionEx.ThrowServiceException(ex);
+                }
+            }
+        }
+
+        #endregion
+
+    }
+}
