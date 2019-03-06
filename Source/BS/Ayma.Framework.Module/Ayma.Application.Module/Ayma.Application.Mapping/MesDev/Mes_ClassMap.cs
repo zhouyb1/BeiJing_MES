dc6337f0a276827daf079acebcd05978b171900a
周雅波 <1815674+zybanswer@users.noreply.gitee.com>
2019-03-06 14:14:26 +0800

    no commit message

---
+++ Source/BS/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.Mapping/MesDev/Mes_ClassMap.cs
@@ -0,0 +1,27 @@
+﻿using Ayma.Application.TwoDevelopment.MesDev;
+using System.Data.Entity.ModelConfiguration;
+
+namespace  Ayma.Application.Mapping
+{
+    /// <summary>
+    /// 创 建：超级管理员
+    /// 日 期：2019-03-06 13:52
+    /// 描 述：排班管理
+    /// </summary>
+    public partial class Mes_ClassMap : EntityTypeConfiguration<Mes_ClassEntity>
+    {
+        public Mes_ClassMap()
+        {
+            #region 表、主键
+            //表
+            this.ToTable("MES_CLASS");
+            //主键
+            this.HasKey(t => t.ID);
+            #endregion
+
+            #region 配置关系
+            #endregion
+        }
+    }
+}
+
---
+++ Source/BS/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.Mapping/MesDev/Mes_WorkShopMap.cs
@@ -0,0 +1,27 @@
+﻿using Ayma.Application.TwoDevelopment.MesDev;
+using System.Data.Entity.ModelConfiguration;
+
+namespace  Ayma.Application.Mapping
+{
+    /// <summary>
+    /// 创 建：超级管理员
+    /// 日 期：2019-03-06 12:03
+    /// 描 述：车间管理
+    /// </summary>
+    public partial class Mes_WorkShopMap : EntityTypeConfiguration<Mes_WorkShopEntity>
+    {
+        public Mes_WorkShopMap()
+        {
+            #region 表、主键
+            //表
+            this.ToTable("MES_WORKSHOP");
+            //主键
+            this.HasKey(t => t.Id);
+            #endregion
+
+            #region 配置关系
+            #endregion
+        }
+    }
+}
+
