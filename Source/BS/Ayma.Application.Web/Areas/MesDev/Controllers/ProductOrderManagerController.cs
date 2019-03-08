2c039f9babeee525e383fd16556155cb287536a4
周雅波 <1815674+zybanswer@users.noreply.gitee.com>
2019-03-08 09:08:16 +0800

    no commit message

---
+++ Source/BS/Ayma.Application.Web/Areas/MesDev/Controllers/ProductOrderManagerController.cs
@@ -0,0 +1,140 @@
+﻿using Ayma.Util;
+using Ayma.Application.TwoDevelopment.MesDev;
+using System.Web.Mvc;
+using System.Collections.Generic;
+
+namespace Ayma.Application.Web.Areas.MesDev.Controllers
+{
+    /// <summary>
+    /// 创 建：超级管理员
+    /// 日 期：2019-03-07 11:05
+    /// 描 述：生产订单管理
+    /// </summary>
+    public partial class ProductOrderManagerController : MvcControllerBase
+    {
+        private ProductOrderManagerIBLL productOrderManagerIBLL = new ProductOrderManagerBLL();
+
+        #region 视图功能
+
+        /// <summary>
+        /// 主页面
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Index()
+        {
+             return View();
+        }
+        /// <summary>
+        /// 表单页
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Form()
+        {
+             return View();
+        }
+        #endregion
+
+        #region 获取数据
+
+        /// <summary>
+        /// 获取页面显示列表数据
+        /// </summary>
+        /// <param name="queryJson">查询参数</param>
+        /// <returns></returns>
+        [HttpGet]
+        [AjaxOnly]
+        public ActionResult GetPageList(string pagination, string queryJson)
+        {
+            Pagination paginationobj = pagination.ToObject<Pagination>();
+            var data = productOrderManagerIBLL.GetPageList(paginationobj, queryJson);
+            var jsonData = new
+            {
+                rows = data,
+                total = paginationobj.total,
+                page = paginationobj.page,
+                records = paginationobj.records
+            };
+            return Success(jsonData);
+        }
+        /// <summary>
+        /// 获取表单数据
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [AjaxOnly]
+        public ActionResult GetFormData(string keyValue)
+        {
+            var Mes_ProductOrderHeadData = productOrderManagerIBLL.GetMes_ProductOrderHeadEntity( keyValue );
+            var Mes_ProductOrderDetailData = productOrderManagerIBLL.GetMes_ProductOrderDetaillist(Mes_ProductOrderHeadData.P_OrderNo);
+            var jsonData = new {
+                Mes_ProductOrderHeadData = Mes_ProductOrderHeadData,
+                Mes_ProductOrderDetailData = Mes_ProductOrderDetailData,
+            };
+            return Success(jsonData);
+        }
+        #endregion
+
+        #region 提交数据
+
+        /// <summary>
+        /// 删除实体数据
+        /// </summary>
+        /// <param name="keyValue">主键</param>
+        /// <returns></returns>
+        [HttpPost]
+        [AjaxOnly]
+        public ActionResult DeleteForm(string keyValue)
+        {
+            productOrderManagerIBLL.DeleteEntity(keyValue);
+            return Success("删除成功！");
+        }
+        /// <summary>
+        /// 保存实体数据（新增、修改）
+        /// </summary>
+        /// <param name="keyValue">主键</param>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AjaxOnly]
+        public ActionResult SaveForm(string keyValue, string strEntity, string strmes_ProductOrderDetaillist)
+        {
+            Mes_ProductOrderHeadEntity entity = strEntity.ToObject<Mes_ProductOrderHeadEntity>();
+            List<Mes_ProductOrderDetailEntity >mes_ProductOrderDetaillist = strmes_ProductOrderDetaillist.ToObject<List<Mes_ProductOrderDetailEntity>>();
+            productOrderManagerIBLL.SaveEntity(keyValue, entity, mes_ProductOrderDetaillist);
+            return Success("保存成功！");
+        }
+        /// <summary>
+        /// 审核单据
+        /// </summary>
+        /// <param name="keyValue"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [AjaxOnly]
+        public ActionResult AuditingBill(string keyValue)
+        {
+            productOrderManagerIBLL.AuditingBill(keyValue);
+            return Success("审核成功！");
+        }
+
+        /// <summary>
+        /// 原辅料统计页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult BomPartSum()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AjaxOnly]
+        public ActionResult GetBomRecordTreeList()
+        {
+            return Fail("");
+        }
+
+        #endregion
+
+    }
+}
