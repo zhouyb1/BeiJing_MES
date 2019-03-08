3d5bfd662dc90a1348a234856549facc2579b9a6
周雅波 <1815674+zybanswer@users.noreply.gitee.com>
2019-03-08 17:03:08 +0800

    no commit message

---
+++ Source/BS/Ayma.Application.Web/Areas/MesDev/Views/ProductOrderManager/Form.js
@@ -0,0 +1,90 @@
+﻿/* * 创建人：超级管理员
+ * 日  期：2019-03-07 11:05
+ * 描  述：生产订单管理
+ */
+var acceptClick;
+var keyValue = request('keyValue');
+var bootstrap = function ($, ayma) {
+    "use strict";
+    var selectedRow = ayma.frameTab.currentIframe().selectedRow;
+    var page = {
+        init: function () {
+            $('.am-form-wrap').mCustomScrollbar({ theme: "minimal-dark" });
+            page.bind();
+            page.initData();
+        },
+        bind: function () {
+            //$('#am_form_tabs').FormTab();
+            //$('#am_form_tabs ul li').eq(0).trigger('click');
+            $('#P_Status').DataItemSelect({ code: 'ProductOrderStatus' });
+
+            $('#Mes_ProductOrderDetail').jfGrid({
+                headData: [
+                     {
+                         label: 'ID', name: 'ID', width: 160, align: 'left', editType: 'label', hidden: true
+                     },
+                    {
+                        label: '物料编码', name: 'P_GoodsCode', width: 140, align: 'left', editType: 'label'
+                    },
+                    {
+                        label: '物料名称', name: 'P_GoodsName', width: 140, align: 'left', editType: 'label'
+                    },
+                    {
+                        label: '单位', name: 'P_Unit', width: 60, align: 'left', editType: 'label'
+                    },
+                     {
+                         label: '数量', name: 'P_Qty', width: 60, align: 'left', editType: 'input',
+                         editOp: {
+                             callback: function (rownum, row) {
+                                 if (/\D/.test(row.P_Qty.toString().replace('.', ''))) { //验证只能为数字
+                                     row.M_Qty = 0;
+                                 }
+
+                             }
+                         }
+                     },
+                    {
+                        label: '订单时间', name: 'P_OrderDate', width: 100, align: 'left', editType: 'label'
+                    }
+                ],
+                isAutoHeight: false,
+                footerrow: true,
+                minheight: 400,
+                isEidt: true,
+                isMultiselect: true,
+                height: 300,
+                //inputCount: 2
+            });
+        },
+        initData: function () {
+            if (!!keyValue) {
+                $.SetForm(top.$.rootUrl + '/MesDev/ProductOrderManager/GetFormData?keyValue=' + keyValue, function (data) {
+                    for (var id in data) {
+                        if (!!data[id].length && data[id].length > 0) {
+                            $('#Mes_ProductOrderDetail').jfGridSet('refreshdata', { rowdatas: data[id] });
+                        }
+                        else {
+                            $('[data-table="' + id + '"]').SetFormData(data[id]);
+                        }
+                    }
+                });
+            }
+        }
+    };
+    // 保存数据
+    acceptClick = function (callBack) {
+        if (!$('body').Validform()) {
+            return false;
+        }
+        var postData = {};
+        postData.strEntity = JSON.stringify($('[data-table="Mes_ProductOrderHead"]').GetFormData());
+        postData.strmes_ProductOrderDetaillist = JSON.stringify($('#Mes_ProductOrderDetail').jfGridGet('rowdatas'));
+        $.SaveForm(top.$.rootUrl + '/MesDev/ProductOrderManager/SaveForm?keyValue=' + keyValue, postData, function (res) {
+            // 保存成功后才回调
+            if (!!callBack) {
+                callBack();
+            }
+        });
+    };
+    page.init();
+}
---
+++ Source/BS/Ayma.Application.Web/Areas/MesDev/Views/ProductOrderManager/Index.js
@@ -0,0 +1,171 @@
+﻿/* * 创建人：超级管理员
+ * 日  期：2019-03-07 11:05
+ * 描  述：生产订单管理
+ */
+var refreshGirdData;
+var bootstrap = function ($, ayma) {
+    "use strict";
+    var page = {
+        init: function () {
+            page.initGird();
+            page.bind();
+            page.dbClick();
+        },
+        bind: function () {
+            $('#multiple_condition_query').MultipleQuery(function (queryJson) {
+                page.search(queryJson);
+            }, 220, 400);
+            // 刷新
+            $('#am_refresh').on('click', function () {
+                location.reload();
+            });
+
+            // 编辑
+            $('#am_edit').on('click', function () {
+                var keyValue = $('#girdtable').jfGridValue('ID');
+                if (ayma.checkrow(keyValue)) {
+                    ayma.layerForm({
+                        id: 'form',
+                        title: '编辑',
+                        url: top.$.rootUrl + '/MesDev/ProductOrderManager/Form?keyValue=' + keyValue,
+                        width: 800,
+                        height: 600,
+                        maxmin: true,
+                        callBack: function (id) {
+                            return top[id].acceptClick(refreshGirdData);
+                        }
+                    });
+                }
+            });
+            //审核
+            $('#am_auditing').on('click', function() {
+                var keyValue = $('#girdtable').jfGridValue('ID');
+                var p_status = $('#girdtable').jfGridValue('P_Status');
+                if (p_status=="2") {
+                    ayma.alert.error("已审核");
+                    return false;
+                }
+                if (p_status=="0") {
+                    ayma.alert.error("处于生产计划中");
+                    return false;
+                }
+                if (ayma.checkrow(keyValue)) {
+                    ayma.layerConfirm('是否确认审核单据', function(res) {
+                        if (res) {
+                            ayma.postForm(top.$.rootUrl + '/MesDev/ProductOrderManager/AuditingBill?keyValue=' + keyValue, { keyValue: keyValue }, function() {
+                                refreshGirdData();
+                            });
+                        }
+                    });
+                }
+            });
+
+            //统计
+            $('#am_sum').on('click', function() {
+                var keyValue = $('#girdtable').jfGridValue('ID');
+                var p_status = $('#girdtable').jfGridValue('P_Status');
+                var code = $('#girdtable').jfGridValue('P_GoodsCode');
+                if (p_status == "0") {
+                    ayma.alert.error("订单处于生成计划中！");
+                    return false;
+                }
+                if (p_status=="1") {
+                    ayma.alert.error("请先审核订单！");
+                    return false;
+                }
+                if (ayma.checkrow(keyValue)) {
+                    //打开统计页面BomPartSum 
+                    ayma.layerForm({
+                        id: 'form',
+                        title: '编辑',
+                        url: top.$.rootUrl + '/MesDev/ProductOrderManager/BomPartSum?GoodsCode=' + code,
+                        width: 800,
+                        height: 600,
+                        maxmin: true,
+                        callBack: function(id) {
+                            return top[id].acceptClick(refreshGirdData);
+                        }
+                    });
+                }
+            });
+            // 删除
+            $('#am_delete').on('click', function () {
+                var keyValue = $('#girdtable').jfGridValue('ID');
+                if (ayma.checkrow(keyValue)) {
+                    ayma.layerConfirm('是否确认删除该项！', function (res) {
+                        if (res) {
+                            ayma.deleteForm(top.$.rootUrl + '/MesDev/ProductOrderManager/DeleteForm', { keyValue: keyValue }, function () {
+                                refreshGirdData();
+                            });
+                        }
+                    });
+                }
+            });
+        },
+        // 初始化列表
+        initGird: function () {
+            $('#girdtable').AuthorizeJfGrid({
+                url: top.$.rootUrl + '/MesDev/ProductOrderManager/GetPageList',
+                headData: [
+                    { label: "生产订单号", name: "P_OrderNo", width: 160, align: "left" },
+                    { label: "订单时间", name: "P_OrderDate", width: 160, align: "left" },
+                    { label: "使用时间", name: "P_UseDate", width: 160, align: "left" },
+                    { label: "物料编码", name: "P_GoodsCode", width: 160, align: "left" },
+                    { label: "数量", name: "P_Qty", width: 160, align: "left" },
+                    { label: "单位", name: "P_Unit", width: 160, align: "left" },
+                    { label: "物料名称", name: "P_GoodsName", width: 160, align: "left" },
+                    {
+                        label: "状态", name: "P_Status", width: 160, align: "left",
+                        formatterAsync: function (callback, value, row) {
+                            ayma.clientdata.getAsync('dataItem', {
+                                key: value,
+                                code: 'ProductOrderStatus',
+                                callback: function(_data) {
+                                    if (value == 0) {
+                                        callback("<span class='label label-default'>" + _data.text + "</span>");
+                                    } else if (value == 1) {
+                                        callback("<span class='label label-info'>" + _data.text + "</span>");
+                                    } else {
+                                        callback("<span class='label label-success'>" + _data.text + "</span>");
+                                    }
+                                }
+                            });
+                        }
+                    }
+                ],
+                mainId: 'ID',
+                reloadSelected: true,
+                isPage: true
+            });
+            page.search();
+        },
+        search: function (param) {
+            param = param || {};
+            $('#girdtable').jfGridSet('reload', { param: { queryJson: JSON.stringify(param) } });
+        },
+
+        dbClick: function () {
+            $('#girdtable').on('dblclick', function () {
+                var keyValue = $('#girdtable').jfGridValue('ID');
+                if (ayma.checkrow(keyValue)) {
+                    ayma.layerForm({
+                        id: 'form',
+                        title: '编辑',
+                        url: top.$.rootUrl + '/MesDev/ProductOrderManager/Form?keyValue=' + keyValue,
+                        width: 600,
+                        height: 400,
+                        maxmin: true,
+                        callBack: function (id) {
+                            return top[id].acceptClick(refreshGirdData);
+                        }
+                    });
+                }
+
+            });
+        }
+    };
+    refreshGirdData = function () {
+        page.search();
+    };
+    page.init();
+}
