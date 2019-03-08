625616172b451a89646098526d1d1d97df38e427
邓志康 <1128983+luckykang@users.noreply.gitee.com>
2019-03-08 17:40:33 +0800

    no commit message

--- Source/BS/Ayma.Application.Web/Areas/MesDev/Views/ProductOrderManager/BomPartSum.js
+++ Source/BS/Ayma.Application.Web/Areas/MesDev/Views/ProductOrderManager/BomPartSum.js
@@ -1,4 +1,4 @@
-﻿var parentId;
+﻿
 var goodsCode = request('GoodsCode');
 
 var bootstrap = function ($, ayma) {
@@ -8,7 +8,7 @@ var bootstrap = function ($, ayma) {
         init: function () {
             $('.am-form-wrap').mCustomScrollbar({ theme: "minimal-dark" });
             page.bind();
-            page.initGird();
+            //page.initGird();
         },
         bind: function () {
             $.post(top.$.rootUrl + '/MesDev/Tools/GetCode', { goodsCode: goodsCode }, function (res) {
@@ -24,21 +24,18 @@ var bootstrap = function ($, ayma) {
             }, 'json');
             $('#B_ParentID').on('change', function () {
                 var parentId = $(this).selectGet();
-                page.initGird();
+
                 page.initData(parentId);
 
             });
-        },
-        
-        initGird: function () {
             $('#girdtable').jfGrid({
                 headData: [
                     { label: "工艺代码", name: "B_RecordCode", width: 160, align: "left" },
-                    { label: "工序号", name: "B_ProNo", width: 160, align: "left" },
-                    { label: "配方编码", name: "B_FormulaCode", width: 160, align: "left" },
-                    { label: "配方名称", name: "B_FormulaName", width: 160, align: "left" },
                     { label: "物料编码", name: "B_GoodsCode", width: 160, align: "left" },
                     { label: "物料名称", name: "B_GoodsName", width: 160, align: "left" },
+                     { label: "配方编码", name: "B_FormulaCode", width: 160, align: "left" },
+                    { label: "配方名称", name: "B_FormulaName", width: 160, align: "left" },
+                    { label: "工序号", name: "B_ProNo", width: 160, align: "left" },
                     { label: '单位', name: 'B_Unit', width: 100, align: 'left' },
                     { label: '数量', name: 'B_Qty', width: 160, align: 'left' },
                 ],
@@ -46,22 +43,23 @@ var bootstrap = function ($, ayma) {
                 mainId: 'ID',
                 parentId: 'B_ParentID',
                 reloadSelected: true,
-                isShowNum: true
+                isShowNum: true,
+                height: 400,
+                //minheight: 400,
+                //isAutoHeight: false
                 //isAutoHeight: true,          // 自动适应表格高度
             });
         },
+        
         initData: function (parentId) {
             $.SetForm(top.$.rootUrl + '/MesDev/ProductOrderManager/GetBomTreeList?parentId=' + parentId, function (data) {
-                for (var id in data) {
-                    debugger;
-                    $('#girdtable').jfGridSet('refreshdata', { rowdatas: data[id]});
+                console.log(data)
+                $('#girdtable').jfGridSet('refreshdata', { rowdatas: data});
+                //for (var id in data) {
+                //    $('#girdtable').jfGridSet('refreshdata', { rowdatas: data.data[id] });
 
-                }
+                //}
             });
-        },
-        search: function (data) {
-            data = data || {};
-            $('#girdtable').jfGridSet('refreshdata', { rowdatas: data });
         }
         
 
