c9a8ebfa4cef36cf4ae28805d62b70a21778a49f
周雅波 <1815674+zybanswer@users.noreply.gitee.com>
2019-03-08 16:49:09 +0800

    no commit message

--- Source/BS/Ayma.Application.Web/Areas/MesDev/Views/ProductOrderManager/BomPartSum.js
+++ Source/BS/Ayma.Application.Web/Areas/MesDev/Views/ProductOrderManager/BomPartSum.js
@@ -1,72 +1,70 @@
-﻿var b_parentId = request('');
-var g_code = requestStart('');
-//var selectedRow = top.BomRecordIndexSelectedRow();
+﻿var parentId;
+var goodsCode = request('GoodsCode');
 
-var keyValue = '';
 var bootstrap = function ($, ayma) {
     "user strict";
     var page = {
-        init: function() {
+       
+        init: function () {
+            $('.am-form-wrap').mCustomScrollbar({ theme: "minimal-dark" });
             page.bind();
             page.initGird();
         },
-        bind: function() {
-            $('#B_Avail').DataItemSelect({ code: 'YesOrNo' });
-            $('#B_ParentID').select({
-                url: top.$.rootUrl + '/MesDev/Tools/GetBomRecordTree',
-                type: 'tree',
-                allowSearch: true,
-                maxHeight: 225
-            }).selectSet(b_parentId);
-
-            $('#B_ProNo').select(); //下拉初始化
-            //工艺代码
-            $("#B_RecordCode").select({
-                type: 'default',
-                value: 'P_RecordCode',
-                text: 'P_RecordCode',
-                // 展开最大高度
-                maxHeight: 200,
-                // 是否允许搜索
-                allowSearch: true,
-                // 访问数据接口地址
-                url: top.$.rootUrl + '/MesDev/Tools/GetProceList',
-                // 访问数据接口参数
-                param: { parentId: "0" }
-            }).on('change', function() {
-                var code = $(this).selectGet();
-                $("#B_ProNo").selectRefresh({
+        bind: function () {
+            $.post(top.$.rootUrl + '/MesDev/Tools/GetCode', { goodsCode: goodsCode }, function (res) {
+                $('#B_ParentID').select({
+                    url: top.$.rootUrl + '/MesDev/Tools/GetBomList',
                     type: 'default',
-                    value: 'P_ProNo',
-                    text: 'P_ProNo',
-                    // 展开最大高度
-                    maxHeight: 200,
-                    // 是否允许搜索
-                    allowSearch: true,
-                    // 访问数据接口地址
-                    url: top.$.rootUrl + '/MesDev/Tools/GetProceListBy',
-                    // 访问数据接口参数
-                    param: { code: code }
+                    value: 'ID',
+                    text: "B_FormulaName",
+                    maxHeight: 225,
+                    param: { goodsCode: res.data.G_Code }
                 });
+
+            }, 'json');
+            $('#B_ParentID').on('change', function () {
+                var parentId = $(this).selectGet();
+                page.initGird();
+                page.initData(parentId);
+
             });
         },
-        //initData: function() {
-        //    if (!!selectedRow) {
-        //        keyValue = selectedRow.ID || '';
-        //        $('#form').SetFormData(selectedRow);
-
-        //    } else {
-        //        $('#B_Avail').selectSet(1);
-        //    }
-        //},
-
-        initGird: function() {
-            $('#gridtable').jfGrid({
-                url: top.$.rootUrl + '/MesDev/ProductOrderManager/GetBomRecordTreeList'
+        
+        initGird: function () {
+            $('#girdtable').jfGrid({
+                headData: [
+                    { label: "工艺代码", name: "B_RecordCode", width: 160, align: "left" },
+                    { label: "工序号", name: "B_ProNo", width: 160, align: "left" },
+                    { label: "配方编码", name: "B_FormulaCode", width: 160, align: "left" },
+                    { label: "配方名称", name: "B_FormulaName", width: 160, align: "left" },
+                    { label: "物料编码", name: "B_GoodsCode", width: 160, align: "left" },
+                    { label: "物料名称", name: "B_GoodsName", width: 160, align: "left" },
+                    { label: '单位', name: 'B_Unit', width: 100, align: 'left' },
+                    { label: '数量', name: 'B_Qty', width: 160, align: 'left' },
+                ],
+                isTree: true,
+                mainId: 'ID',
+                parentId: 'B_ParentID',
+                reloadSelected: true,
+                isShowNum: true
+                //isAutoHeight: true,          // 自动适应表格高度
             });
+        },
+        initData: function (parentId) {
+            $.SetForm(top.$.rootUrl + '/MesDev/ProductOrderManager/GetBomTreeList?parentId=' + parentId, function (data) {
+                for (var id in data) {
+                    debugger;
+                    $('#girdtable').jfGridSet('refreshdata', { rowdatas: data[id]});
+
+                }
+            });
+        },
+        search: function (data) {
+            data = data || {};
+            $('#girdtable').jfGridSet('refreshdata', { rowdatas: data });
         }
+        
+
     };
-    //top.BomRecordIndexSelectedRow = function () {
-    //    return selectedRow;
-    //}
+    page.init();
 }
