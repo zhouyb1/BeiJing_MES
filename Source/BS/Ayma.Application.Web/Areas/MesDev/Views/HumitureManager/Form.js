7dfbc4190506cd6f24c53486f230c6247754ebd0
周雅波 <1815674+zybanswer@users.noreply.gitee.com>
2019-03-11 17:53:40 +0800

    no commit message

--- Source/BS/Ayma.Application.Web/Areas/MesDev/Views/HumitureManager/Form.js
+++ Source/BS/Ayma.Application.Web/Areas/MesDev/Views/HumitureManager/Form.js
@@ -15,6 +15,36 @@ $('.am-form-wrap').mCustomScrollbar({theme: "minimal-dark"});
         },
         bind: function () {
             $('#H_Kind').DataItemSelect({ code: 'HumitureType' });
+            //上下值校验
+            $("#H_Up").on('keyup', function() {
+                var h_up = $.trim($(this).val()); //去除空格
+                var h_down = $.trim($("#H_Down").val());
+                var html = '<div class="am-field-error-info" id="isCode" title="上限值不能小于上限值！"><i class="fa fa-info-circle"></i></div>';
+
+                if (parseFloat(h_up) <parseFloat(h_down)) {
+                    $("#H_Up").addClass("am-field-error");
+                    $("#H_Up").parent().append(html);
+                    ayma.alert.error("上限值不能小于下限值");
+                } else {
+                    $("#H_Up").removeClass("am-field-error");
+                    $("#isCode").remove();
+                }
+            });
+
+
+            $("#H_Down").on('keyup', function() {
+                var html = '<div class="am-field-error-info" id="isCode" title="下限值不能大于上限值！"><i class="fa fa-info-circle"></i></div>';
+                var h_down = $.trim($(this).val()); //去除空格
+                var h_up = $.trim($("#H_Up").val());
+                if (parseFloat(h_up) < parseFloat(h_down)) {
+                    $("#H_Down").addClass("am-field-error");
+                    $("#H_Down").parent().append(html);
+                    ayma.alert.error("下限值不能大于上限值");
+                } else {
+                    $("#H_Down").removeClass("am-field-error");
+                    $("#isCode").remove();
+                }
+            });
         },
         initData: function () {
             if (!!keyValue) {
