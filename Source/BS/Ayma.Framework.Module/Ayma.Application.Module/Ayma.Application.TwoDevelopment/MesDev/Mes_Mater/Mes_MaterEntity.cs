8cb7010bb9569fcb28d1d241cb0fe12ee3d1e824
周雅波 <1815674+zybanswer@users.noreply.gitee.com>
2019-03-11 10:28:12 +0800

    生产订单原物料

---
+++ Source/BS/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.TwoDevelopment/MesDev/Mes_Mater/Mes_MaterEntity.cs
@@ -0,0 +1,78 @@
+﻿using Ayma.Util;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Ayma.Application.TwoDevelopment.MesDev
+
+{
+    /// <summary>
+    /// 创 建：超级管理员
+    /// 日 期：2019-03-10 20:23
+    /// 描 述：生产订单原物料需求表
+    /// </summary>
+    public partial class Mes_MaterEntity 
+    {
+        #region 实体成员
+        /// <summary>
+        /// ID
+        /// </summary>
+        /// <returns></returns>
+        [Column("ID")]
+        public string ID { get; set; }
+        /// <summary>
+        /// 生产订单号
+        /// </summary>
+        /// <returns></returns>
+        [Column("P_ORDERNO")]
+        public string P_OrderNo { get; set; }
+        /// <summary>
+        /// 订单时间
+        /// </summary>
+        /// <returns></returns>
+        [Column("P_ORDERDATE")]
+        public DateTime? P_OrderDate { get; set; }
+        /// <summary>
+        /// 物料编码
+        /// </summary>
+        /// <returns></returns>
+        [Column("P_GOODSCODE")]
+        public string P_GoodsCode { get; set; }
+        /// <summary>
+        /// 物料名称
+        /// </summary>
+        /// <returns></returns>
+        [Column("P_GOODSNAME")]
+        public string P_GoodsName { get; set; }
+        /// <summary>
+        /// 单位
+        /// </summary>
+        /// <returns></returns>
+        [Column("P_UNIT")]
+        public string P_Unit { get; set; }
+        /// <summary>
+        /// 数量
+        /// </summary>
+        /// <returns></returns>
+        [Column("P_QTY")]
+        public decimal? P_Qty { get; set; }
+        #endregion
+
+        #region 扩展操作
+        /// <summary>
+        /// 新增调用
+        /// </summary>
+        public void Create()
+        {
+            this.ID = Guid.NewGuid().ToString();
+        }
+        /// <summary>
+        /// 编辑调用
+        /// </summary>
+        /// <param name="keyValue"></param>
+        public void Modify(string keyValue)
+        {
+            this.ID = keyValue;
+        }
+        #endregion
+    }
+}
+
