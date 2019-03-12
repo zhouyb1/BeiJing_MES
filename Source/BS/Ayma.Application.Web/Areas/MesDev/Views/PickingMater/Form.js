af1b52ab5e977a1a8e0eebc263fa18a6489c1f32
周雅波 <1815674+zybanswer@users.noreply.gitee.com>
2019-03-12 17:28:16 +0800

    no commit message

---
+++ Source/BS/Ayma.Application.Web/Areas/MesDev/Views/PickingMater/Form.js
@@ -0,0 +1,169 @@
+﻿/* * 创建人：超级管理员
+ * 日  期：2019-03-11 19:22
+ * 描  述：领料单
+ */
+var acceptClick;
+var refreshGirdData;//表格商品添加
+var RemoveGridData;//移除表格
+var parentFormId = request('formId');//父级窗口id
+var keyValue = request('keyValue');
+var tmp = new Map();
+var bootstrap = function ($, ayma) {
+    "use strict";
+    var selectedRow = ayma.frameTab.currentIframe().selectedRow;
+    var page = {
+        init: function () {
+$('.am-form-wrap').mCustomScrollbar({theme: "minimal-dark"}); 
+            page.bind();
+            page.initData();
+        },
+        bind: function() {
+            //绑定仓库
+            var dfop = {
+                type: 'default',
+                value: 'S_Code',
+                text: 'S_Code',
+                // 展开最大高度
+                maxHeight: 200,
+                // 是否允许搜索
+                allowSearch: true,
+                // 访问数据接口地址
+                url: top.$.rootUrl + '/MesDev/Tools/GetStockList',
+                // 访问数据接口参数
+                param: {}
+            }
+            $("#C_StockCode").select(dfop).on('change', function() {
+                var code = $(this).selectGet();
+                $.ajax({
+                    type: "get",
+                    url: top.$.rootUrl + '/MesDev/Tools/ByCodeGetStockEntity',
+                    data: { code: code },
+                    success: function(data) {
+                        var entity = JSON.parse(data).data;
+                        $("#C_StockName").val(entity.S_Name);
+                    }
+                });
+            });
+
+            //绑定目标仓
+            $("#C_StockToCode").select(dfop).on('change', function() {
+                var code = $(this).selectGet();
+                $.ajax({
+                    type: "get",
+                    url: top.$.rootUrl + '/MesDev/Tools/ByCodeGetStockEntity',
+                    data: { code: code },
+                    success: function(data) {
+                        var entity = JSON.parse(data).data;
+                        $("#C_StockToName").val(entity.S_Name);
+                    }
+                });
+            });
+            //添加订单原物料
+            $('#am_add').on('click', function() {
+                ayma.layerForm({
+                    id: 'OrderMaterListForm',
+                    title: '添加订单物料',
+                    url: top.$.rootUrl + '/MesDev/PickingMater/OrderMaterList?formId=' + parentFormId,
+                    width: 700,
+                    height: 500,
+                    maxmin: true,
+                    callback: function(id, index) {
+                        return top[id].closeWindow();
+                    }
+                });
+            });
+            $('#Mes_CollarDetail').jfGrid({
+                headData: [
+                    { label: "领料单", name: "C_CollarNo", width: 130, align: "left", hidden: true },
+                    { label: "生产订单", name: "C_OrderNo", width: 130, align: "left", hidden: true },
+                    { label: "物料编码", name: "C_GoodsCode", width: 130, align: "left"},
+                    { label: "物料名称", name: "C_GoodsName", width: 130, align: "left" },
+                    { label: "单位", name: "C_Unit", width: 60, align: "left" },
+                    { label: "数量", name: "C_Qty", width: 60, align: "left", editType: 'input' }
+                    //{ label: "批次", name: "C_Batch", width: 60, align: "left" }
+                ],
+                isAutoHeight: false,
+                footerrow: true,
+                minheight: 400,
+                isEidt: true,
+                isMultiselect: true,
+                height: 300,
+                inputCount: 2
+            });
+        },
+        initData: function () {
+            if (!!keyValue) {
+                $.SetForm(top.$.rootUrl + '/MesDev/PickingMater/GetFormData?keyValue=' + keyValue, function (data) {
+                    for (var id in data) {
+                        if (!!data[id].length && data[id].length > 0) {
+                            $('#Mes_CollarDetail').jfGridSet('refreshdata', { rowdatas: data[id] });
+                        }
+                        else {
+                            $('[data-table="' + id + '"]').SetFormData(data[id]);
+                        }
+                    }
+                });
+            }
+        },
+        search: function (data) {
+            data = data || {};
+            $('#Mes_CollarDetail').jfGridSet('refreshdata', { rowdatas: data });
+        }
+
+    };
+    // 保存数据
+    acceptClick = function (callBack) {
+        if (!$('body').Validform()) {
+            return false;
+        }
+        var postData = {};
+        postData.strEntity = JSON.stringify($('[data-table="Mes_CollarHead"]').GetFormData());
+        postData.strmes_CollarDetailEntity = JSON.stringify($('#Mes_CollarDetail').jfGridGet('rowdatas'));
+        $.SaveForm(top.$.rootUrl + '/MesDev/PickingMater/SaveForm?keyValue=' + keyValue, postData, function (res) {
+            // 保存成功后才回调
+            if (!!callBack) {
+                callBack();
+            }
+        });
+    };
+    //表格商品添加
+    refreshGirdData = function (data, row) {
+        $('#P_OrderNo').val(row.C_OrderNo);
+        $('#P_OrderDate').val(row.C_OrderDate);
+        var rows = $('#Mes_CollarDetail').jfGridGet('rowdatas');
+        if (data.length == 0) { //单选
+            if (!tmp.get(row)) {
+                tmp.set(row, 1);
+                rows.push(row);
+            }
+        } else { //多选                  
+            for (var i = 0; i < data.length; i++) {
+                if (!tmp.get(data[i])) {
+                    tmp.set(data[i], 1);
+                    rows.push(data[i]);
+                }
+            }
+        }
+        //数组过滤
+        var filterarray = $.grep(rows, function (item) {
+            return item["C_GoodsCode"] != undefined;
+        });
+        page.search(filterarray);
+    };
+    //表格商品删除
+    RemoveGridData = function (row) {
+        var rows = $('#Mes_CollarDetail').jfGridGet('rowdatas');
+
+        for (var i = 0; i < rows.length; i++) {
+            if (rows[i]["M_GoodsCode"] == row["P_GoodsCode"]) {
+                rows.splice(i, 1);
+                tmp.delete(row);
+                page.search(rows);
+            }
+        }
+    };
+    top.NewGirdData = function () {
+        return $('#Mes_CollarDetail').jfGridGet('rowdatas');
+    }
+    page.init();
+}
---
+++ Source/BS/Ayma.Application.Web/Areas/MesDev/Views/PickingMater/Index.js
@@ -0,0 +1,164 @@
+﻿/* * 创建人：超级管理员
+ * 日  期：2019-03-11 19:22
+ * 描  述：领料单
+ */
+var refreshGirdData;
+var bootstrap = function ($, ayma) {
+    "use strict";
+    var startTime;
+    var endTime;
+    var page = {
+        init: function () {
+            page.initGird();
+            page.bind();
+        },
+        bind: function () {
+            // 时间搜索框
+            $('#datesearch').amdate({
+                dfdata: [
+                    { name: '今天', begin: function () { return ayma.getDate('yyyy-MM-dd 00:00:00') }, end: function () { return ayma.getDate('yyyy-MM-dd 23:59:59') } },
+                    { name: '近7天', begin: function () { return ayma.getDate('yyyy-MM-dd 00:00:00', 'd', -6) }, end: function () { return ayma.getDate('yyyy-MM-dd 23:59:59') } },
+                    { name: '近1个月', begin: function () { return ayma.getDate('yyyy-MM-dd 00:00:00', 'm', -1) }, end: function () { return ayma.getDate('yyyy-MM-dd 23:59:59') } },
+                    { name: '近3个月', begin: function () { return ayma.getDate('yyyy-MM-dd 00:00:00', 'm', -3) }, end: function () { return ayma.getDate('yyyy-MM-dd 23:59:59') } }
+                ],
+                // 月
+                mShow: false,
+                premShow: false,
+                // 季度
+                jShow: false,
+                prejShow: false,
+                // 年
+                ysShow: false,
+                yxShow: false,
+                preyShow: false,
+                yShow: false,
+                // 默认
+                dfvalue: '1',
+                selectfn: function (begin, end) {
+                    startTime = begin;
+                    endTime = end;
+                    page.search();
+                }
+            });
+            $('#multiple_condition_query').MultipleQuery(function (queryJson) {
+                page.search(queryJson);
+            }, 180, 500);
+            // 刷新
+            $('#am_refresh').on('click', function () {
+                location.reload();
+            });
+            // 新增
+            $('#am_add').on('click', function () {
+                ayma.layerForm({
+                    id: 'PickMaterForm',
+                    title: '新增',
+                    url: top.$.rootUrl + '/MesDev/PickingMater/Form?formId=PickMaterForm',
+                    width: 800,
+                    height: 600,
+                    maxmin: true,
+                    callBack: function (id) {
+                        return top[id].acceptClick(refreshGirdData);
+                    }
+                });
+            });
+            // 编辑
+            $('#am_edit').on('click', function () {
+                var keyValue = $('#girdtable').jfGridValue('ID');
+                if (ayma.checkrow(keyValue)) {
+                    ayma.layerForm({
+                        id: 'form',
+                        title: '编辑',
+                        url: top.$.rootUrl + '/MesDev/PickingMater/Form?keyValue=' + keyValue,
+                        width: 600,
+                        height: 400,
+                        maxmin: true,
+                        callBack: function (id) {
+                            return top[id].acceptClick(refreshGirdData);
+                        }
+                    });
+                }
+            });
+            // 删除
+            $('#am_delete').on('click', function () {
+                var keyValue = $('#girdtable').jfGridValue('ID');
+                if (ayma.checkrow(keyValue)) {
+                    ayma.layerConfirm('是否确认删除该项！', function (res) {
+                        if (res) {
+                            ayma.deleteForm(top.$.rootUrl + '/MesDev/PickingMater/DeleteForm', { keyValue: keyValue}, function () {
+                                refreshGirdData();
+                            });
+                        }
+                    });
+                }
+            });
+            //审核单据
+            $("#am_auditing").on('click', function () {
+                var keyValue = $("#girdtable").jfGridValue("ID");
+                var status = $("#girdtable").jfGridValue("M_Status");
+                if (status != "1") {
+                    ayma.alert.error("已审核");
+                    return false;
+                }
+                if (ayma.checkrow(keyValue)) {
+                    ayma.layerConfirm('是否确认审核该单据！', function (res) {
+                        if (res) {
+                            ayma.postForm(top.$.rootUrl + '/MesDev/Tools/AuditingBill', { keyValue: keyValue, tables: 'Mes_CollarHead', field: 'P_Status' }, function () {
+                                refreshGirdData();
+                            });
+                        }
+                    });
+                }
+            });
+        },
+        // 初始化列表
+        initGird: function () {
+            $('#girdtable').AuthorizeJfGrid({
+                url: top.$.rootUrl + '/MesDev/PickingMater/GetPageList',
+                headData: [
+                    {
+                        label: "状态", name: "P_Status", width: 160, align: "left",
+                        formatterAsync: function (callback, value, row) {
+                            ayma.clientdata.getAsync('dataItem', {
+                                key: value,
+                                code: 'RequistStatus',
+                                callback: function (_data) {
+                                    if (value == 1) {
+                                        callback("<span class='label label-default'>" + _data.text + "</span>");
+                                    } else if (value == 2) {
+                                        callback("<span class='label label-info'>" + _data.text + "</span>");
+                                    } else if (value == 3) {
+                                        callback("<span class='label label-success'>" + _data.text + "</span>");
+                                    } else {
+                                        callback("<span class='label label-danger'>" + _data.text + "</span>");
+                                    }
+                                }
+                            });
+                        }
+                    },
+                    { label: "领料单号", name: "C_CollarNo", width: 160, align: "left"},
+                    { label: "原仓库编码", name: "C_StockCode", width: 160, align: "left"},
+                    { label: "原仓库名称", name: "C_StockName", width: 160, align: "left"},
+                    { label: "调拨仓库编码", name: "C_StockToCode", width: 160, align: "left"},
+                    { label: "调拨仓库名称", name: "C_StockToName", width: 160, align: "left"},
+                    { label: "生产订单号", name: "P_OrderNo", width: 160, align: "left"},
+                    { label: "订单时间", name: "P_OrderDate", width: 160, align: "left"},
+                    { label: "备注", name: "C_Remark", width: 160, align: "left"},
+                    { label: "添加人", name: "C_CreateBy", width: 160, align: "left"},
+                    { label: "添加时间", name: "C_CreateDate", width: 160, align: "left"},
+                ],
+                mainId:'ID',
+                reloadSelected: true,
+                isPage: true
+            });
+            page.search();
+        },
+        search: function (param) {
+            param = param || {};
+            $('#girdtable').jfGridSet('reload', { param: { queryJson: JSON.stringify(param) } });
+        }
+    };
+    refreshGirdData = function () {
+        page.search();
+    };
+    page.init();
+}
