3f1112c940ae2edb3ffddd6912c19d9870146efa
周雅波 <1815674+zybanswer@users.noreply.gitee.com>
2019-03-13 11:07:55 +0800

    no commit message

---
+++ Source/BS/Ayma.Application.Web/Areas/MesDev/Controllers/PickingMaterQueryController.cs
@@ -0,0 +1,111 @@
+﻿using Ayma.Util;
+using Ayma.Application.TwoDevelopment.MesDev;
+using System.Web.Mvc;
+using System.Collections.Generic;
+
+namespace Ayma.Application.Web.Areas.MesDev.Controllers
+{
+    /// <summary>
+    /// 创 建：超级管理员
+    /// 日 期：2019-03-13 10:06
+    /// 描 述：领料单查询
+    /// </summary>
+    public partial class PickingMaterQueryController : MvcControllerBase
+    {
+        private PickingMaterQueryIBLL pickingMaterQueryIBLL = new PickingMaterQueryBLL();
+
+        #region 视图功能
+
+        /// <summary>
+        /// 主页面
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Index()
+        {
+             return View();
+        }
+        /// <summary>
+        /// 表单页
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Form()
+        {
+             return View();
+        }
+        #endregion
+
+        #region 获取数据
+
+        /// <summary>
+        /// 获取页面显示列表数据
+        /// </summary>
+        /// <param name="queryJson">查询参数</param>
+        /// <returns></returns>
+        [HttpGet]
+        [AjaxOnly]
+        public ActionResult GetPageList(string pagination, string queryJson)
+        {
+            Pagination paginationobj = pagination.ToObject<Pagination>();
+            var data = pickingMaterQueryIBLL.GetPageList(paginationobj, queryJson);
+            var jsonData = new
+            {
+                rows = data,
+                total = paginationobj.total,
+                page = paginationobj.page,
+                records = paginationobj.records
+            };
+            return Success(jsonData);
+        }
+        /// <summary>
+        /// 获取表单数据
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [AjaxOnly]
+        public ActionResult GetFormData(string keyValue)
+        {
+            var Mes_CollarHeadData = pickingMaterQueryIBLL.GetMes_CollarHeadEntity( keyValue );
+            var Mes_CollarDetailData = pickingMaterQueryIBLL.GetMes_CollarDetailEntityList(Mes_CollarHeadData.C_CollarNo);
+            var jsonData = new {
+                Mes_CollarHeadData = Mes_CollarHeadData,
+                Mes_CollarDetailData = Mes_CollarDetailData,
+            };
+            return Success(jsonData);
+        }
+        #endregion
+
+        #region 提交数据
+
+        /// <summary>
+        /// 删除实体数据
+        /// </summary>
+        /// <param name="keyValue">主键</param>
+        /// <returns></returns>
+        [HttpPost]
+        [AjaxOnly]
+        public ActionResult DeleteForm(string keyValue)
+        {
+            pickingMaterQueryIBLL.DeleteEntity(keyValue);
+            return Success("删除成功！");
+        }
+        /// <summary>
+        /// 保存实体数据（新增、修改）
+        /// </summary>
+        /// <param name="keyValue">主键</param>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AjaxOnly]
+        public ActionResult SaveForm(string keyValue, string strEntity, string strmes_CollarDetailEntity)
+        {
+            Mes_CollarHeadEntity entity = strEntity.ToObject<Mes_CollarHeadEntity>();
+            Mes_CollarDetailEntity mes_CollarDetailEntity = strmes_CollarDetailEntity.ToObject<Mes_CollarDetailEntity>();
+            pickingMaterQueryIBLL.SaveEntity(keyValue,entity,mes_CollarDetailEntity);
+            return Success("保存成功！");
+        }
+        #endregion
+
+    }
+}
