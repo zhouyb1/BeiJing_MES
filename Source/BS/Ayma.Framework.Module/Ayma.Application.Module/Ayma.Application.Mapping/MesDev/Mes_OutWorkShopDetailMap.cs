86d2f22d941039575f48c63a5f0bdffece708b98
周雅波 <1815674+zybanswer@users.noreply.gitee.com>
2019-03-14 10:15:47 +0800

    no commit message

---
+++ Source/BS/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.Mapping/MesDev/Mes_OutWorkShopDetailMap.cs
@@ -0,0 +1,27 @@
+﻿using Ayma.Application.TwoDevelopment.MesDev;
+using System.Data.Entity.ModelConfiguration;
+
+namespace  Ayma.Application.Mapping
+{
+    /// <summary>
+    /// 创 建：超级管理员
+    /// 日 期：2019-03-13 11:57
+    /// 描 述：出库单制作
+    /// </summary>
+    public partial class Mes_OutWorkShopDetailMap : EntityTypeConfiguration<Mes_OutWorkShopDetailEntity>
+    {
+        public Mes_OutWorkShopDetailMap()
+        {
+            #region 表、主键
+            //表
+            this.ToTable("MES_OUTWORKSHOPDETAIL");
+            //主键
+            this.HasKey(t => t.ID);
+            #endregion
+
+            #region 配置关系
+            #endregion
+        }
+    }
+}
+
