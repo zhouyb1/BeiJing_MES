1a47e41d0ac7c125481b99582607d1df6356de0d
周雅波 <1815674+zybanswer@users.noreply.gitee.com>
2019-03-14 18:20:28 +0800

    no commit message

---
+++ Source/BS/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.Mapping/MesDev/Mes_ScrapDetailMap.cs
@@ -0,0 +1,26 @@
+﻿using Ayma.Application.TwoDevelopment.MesDev;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Ayma.Application.Mapping
+{
+    /// <summary>
+    /// 创 建：超级管理员
+    /// 日 期：2019-03-14 11:23
+    /// 描 述：报废单据管理
+    /// </summary>
+    public partial class Mes_ScrapDetailMap : EntityTypeConfiguration<Mes_ScrapDetailEntity>
+    {
+        public Mes_ScrapDetailMap()
+        {
+            #region 表、主键
+            //表
+            this.ToTable("MES_SCRAPDETAIL");
+            //主键
+            this.HasKey(t => t.ID);
+            #endregion
+
+            #region 配置关系
+            #endregion
+        }
+    }
+}
---
+++ Source/BS/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.Mapping/MesDev/Mes_ScrapHeadMap.cs
@@ -0,0 +1,27 @@
+﻿using Ayma.Application.TwoDevelopment.MesDev;
+using System.Data.Entity.ModelConfiguration;
+
+namespace  Ayma.Application.Mapping
+{
+    /// <summary>
+    /// 创 建：超级管理员
+    /// 日 期：2019-03-14 11:20
+    /// 描 述：报废单据管理
+    /// </summary>
+    public partial class Mes_ScrapHeadMap : EntityTypeConfiguration<Mes_ScrapHeadEntity>
+    {
+        public Mes_ScrapHeadMap()
+        {
+            #region 表、主键
+            //表
+            this.ToTable("MES_SCRAPHEAD");
+            //主键
+            this.HasKey(t => t.ID);
+            #endregion
+
+            #region 配置关系
+            #endregion
+        }
+    }
+}
+
