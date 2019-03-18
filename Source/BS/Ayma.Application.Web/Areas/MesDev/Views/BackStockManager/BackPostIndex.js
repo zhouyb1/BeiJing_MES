b6bb0a9ee47a48aa778bc3f728420224fec04f08
周雅波 <1815674+zybanswer@users.noreply.gitee.com>
2019-03-18 12:02:42 +0800

    退库报表

---
+++ Source/BS/Ayma.Application.Web/Areas/MesDev/Views/BackStockManager/BackPostIndex.js
@@ -0,0 +1,151 @@
+﻿var refreshGirdData;
+var bootstrap = function ($, ayma) {
+    "use strict";
+    var startTime;
+    var endTime;
+    var type;
+    var page = {
+        init: function () {
+            page.initGird();
+            page.bind();
+        },
+        bind: function () {
+            // 时间搜索框
+            $('#datesearch').amdate({
+                dfdata: [
+                    { name: '今天', begin: function () { return ayma.getDate('yyyy-MM-dd 00:00:00') }, end: function () { return ayma.getDate('yyyy-MM-dd 23:59:59') } },
+                    { name: '近7天', begin: function () { return ayma.getDate('yyyy-MM-dd 00:00:00', 'd', -6) }, end: function () { return ayma.getDate('yyyy-MM-dd 23:59:59') } },
+                    { name: '近1个月', begin: function () { return ayma.getDate('yyyy-MM-dd 00:00:00', 'm', -1) }, end: function () { return ayma.getDate('yyyy-MM-dd 23:59:59') } },
+                    { name: '近3个月', begin: function () { return ayma.getDate('yyyy-MM-dd 00:00:00', 'm', -3) }, end: function () { return ayma.getDate('yyyy-MM-dd 23:59:59') } }
+                ],
+                // 月
+                mShow: false,
+                premShow: false,
+                // 季度
+                jShow: false,
+                prejShow: false,
+                // 年
+                ysShow: false,
+                yxShow: false,
+                preyShow: false,
+                yShow: false,
+                // 默认
+                dfvalue: '1',
+                selectfn: function (begin, end) {
+                    startTime = begin;
+                    endTime = end;
+                    type = "report";
+                    page.search();
+                }
+            });
+            $('#multiple_condition_query').MultipleQuery(function (queryJson) {
+                queryJson.type = "report";
+                page.search(queryJson);
+            }, 220, 400);
+            //仓库
+            $('#B_StockName').select({
+                type: 'default',
+                value: 'S_Code',
+                text: 'S_Name',
+                // 展开最大高度
+                maxHeight: 200,
+                // 是否允许搜索
+                allowSearch: true,
+                // 访问数据接口地址
+                url: top.$.rootUrl + '/MesDev/Tools/GetStockList',
+                // 访问数据接口参数
+                param: { code: "StockList", strWhere: "S_Kind =1 " }
+            });
+            $('#B_Status').DataItemSelect({ code: 'MaterInStatus' });
+            // 刷新
+            $('#am_refresh').on('click', function () {
+                location.reload();
+            });
+
+
+            // 查看详情
+            $('#am_detail').on('click', function () {
+                var keyValue = $('#girdtable').jfGridValue('ID');
+                if (ayma.checkrow(keyValue)) {
+                    ayma.layerForm({
+                        id: 'form',
+                        title: '编辑',
+                        url: top.$.rootUrl + '/MesDev/BackStockManager/PostPageForm?keyValue=' + keyValue,
+                        width: 700,
+                        height: 400,
+                        maxmin: true,
+                        btn: null,
+                        callBack: function (id) {
+                            return top[id].acceptClick(refreshGirdData);
+                        }
+                    });
+                }
+            });
+            // 撤销单据
+            $('#am_cancle').on('click', function () {
+                var orderNo = $("#girdtable").jfGridValue("B_BackStockNo");
+                if (ayma.checkrow(orderNo)) {
+                    ayma.layerConfirm('是否确认撤销该单据！', function (res) {
+                        if (res) {
+                            ayma.postForm(top.$.rootUrl + '/MesDev/Tools/PostOrCancelOrDeleteBill', { orderNo: orderNo, proc: 'sp_BackStock_Cancel', type: 2 }, function () {
+                                refreshGirdData();
+                            });
+                        }
+                    });
+                }
+            });
+
+
+        },
+        // 初始化列表
+        initGird: function () {
+            $('#girdtable').AuthorizeJfGrid({
+                url: top.$.rootUrl + '/MesDev/BackStockManager/GetPageList',
+                headData: [
+                    {
+                        label: "状态", name: "B_Status", width: 160, align: "left",
+                        formatterAsync: function (callback, value, row) {
+                            ayma.clientdata.getAsync('dataItem', {
+                                key: value,
+                                code: 'RequistStatus',
+                                callback: function (_data) {
+                                    if (value == 1) {
+                                        callback("<span class='label label-default'>" + _data.text + "</span>");
+                                    } else if (value == 2) {
+                                        callback("<span class='label label-info'>" + _data.text + "</span>");
+                                    } else if (value == 3) {
+                                        callback("<span class='label label-success'>" + _data.text + "</span>");
+                                    } else {
+                                        callback("<span class='label label-danger'>" + _data.text + "</span>");
+                                    }
+                                }
+                            });
+                        }
+                    },
+                    { label: "退仓库单号", name: "B_BackStockNo", width: 160, align: "left" },
+                    { label: "仓库编码", name: "B_StockCode", width: 160, align: "left" },
+                    { label: "仓库名称", name: "B_StockName", width: 160, align: "left" },
+                    { label: "退库仓库编码", name: "B_StockToCode", width: 160, align: "left" },
+                    { label: "退库仓库名称", name: "B_StockToName", width: 160, align: "left" },
+                    { label: "备注", name: "B_Remark", width: 160, align: "left" },
+                    { label: "添加人", name: "B_CreateBy", width: 160, align: "left" },
+                    { label: "添加时间", name: "B_CreateDate", width: 160, align: "left" }
+                ],
+                mainId: 'ID',
+                reloadSelected: true,
+                isPage: true
+            });
+        },
+        search: function (param) {
+            param = param || {};
+            param.StartTime = startTime;
+            param.EndTime = endTime;
+            param.type = type;
+            $('#girdtable').jfGridSet('reload', { param: { queryJson: JSON.stringify(param) } });
+        }
+    };
+    refreshGirdData = function () {
+        page.search();
+    };
+    page.init();
+}
---
+++ Source/BS/Ayma.Application.Web/Areas/MesDev/Views/BackStockManager/PostPageForm.js
@@ -0,0 +1,73 @@
+﻿/* * 创建人：超级管理员
+ * 日  期：2019-03-14 11:20
+ * 描  述：退库单查询
+ */
+
+var keyValue = request('keyValue');
+var tmp = new Map();
+var bootstrap = function ($, ayma) {
+    "use strict";
+    var selectedRow = ayma.frameTab.currentIframe().selectedRow;
+    var page = {
+        init: function () {
+            $('.am-form-wrap').mCustomScrollbar({ theme: "minimal-dark" });
+            page.bind();
+            page.initData();
+        },
+        bind: function () {
+            //绑定仓库
+            var dfop = {
+                type: 'default',
+                value: 'S_Name',
+                text: 'S_Name',
+                // 展开最大高度
+                maxHeight: 200,
+                // 是否允许搜索
+                allowSearch: true,
+                // 访问数据接口地址
+                url: top.$.rootUrl + '/MesDev/Tools/GetStockList',
+                // 访问数据接口参数
+                param: {}
+            };
+            //绑定仓库
+            $('#B_StockName').select(dfop);
+            $('#Mes_BackStockDetail').jfGrid({
+                headData: [
+                     { label: "物料编码", name: "B_GoodsCode", width: 130, align: "left", },
+                    { label: "物料名称", name: "B_GoodsName", width: 130, align: "left" },
+                    { label: "单价", name: "B_Price", width: 130, align: "left" },
+                    { label: "单位", name: "B_Unit", width: 60, align: "left" },
+                    { label: "返回数量", name: "B_Qty", width: 60, align: "left", editType: 'input' },
+                    { label: "批次", name: "B_Batch", width: 60, align: "left" }
+                ],
+                isAutoHeight: false,
+                //minheight: 400,
+                height: 400,
+            });
+        },
+        initData: function () {
+            if (!!keyValue) {
+                $.SetForm(top.$.rootUrl + '/MesDev/BackStockManager/GetFormData?keyValue=' + keyValue, function (data) {
+                    for (var id in data) {
+                        if (!!data[id].length && data[id].length > 0) {
+                            $('#Mes_BackStockDetail').jfGridSet('refreshdata', { rowdatas: data[id] });
+                        }
+                        else {
+                            $('[data-table="' + id + '"]').SetFormData(data[id]);
+                        }
+                    }
+                });
+            }
+        },
+        search: function (data) {
+            data = data || {};
+            $('#Mes_BackStockDetail').jfGridSet('refreshdata', { rowdatas: data });
+        }
+    };
+
+
+    top.NewGirdData = function () {
+        return $('#Mes_BackStockDetail').jfGridGet('rowdatas');
+    }
+    page.init();
+}
