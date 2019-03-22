6b5899e9fb1088fe47a13fa09cf4275e6a53d837
周雅波 <1815674+zybanswer@users.noreply.gitee.com>
2019-03-22 15:18:29 +0800

    生产订单号下拉框

--- Source/BS/Ayma.Application.Web/Areas/MesDev/Views/OrgResManager/Form.js
+++ Source/BS/Ayma.Application.Web/Areas/MesDev/Views/OrgResManager/Form.js
@@ -71,6 +71,20 @@ $('.am-form-wrap').mCustomScrollbar({theme: "minimal-dark"});
             });
             $('#O_ProCode').select();
 
+            $('#O_OrderNo').select({
+                type: 'default',
+                value: 'P_OrderNo',
+                text: 'P_OrderNo',
+                // 展开最大高度
+                maxHeight: 200,
+                // 是否允许搜索
+                allowSearch: true,
+                // 访问数据接口地址
+                url: top.$.rootUrl + '/MesDev/Tools/GetProductOrderList',
+                // 访问数据接口参数
+                param: {}
+            });
+
             //添加物料
             $('#am_add').on('click', function () {
                 ayma.layerForm({
