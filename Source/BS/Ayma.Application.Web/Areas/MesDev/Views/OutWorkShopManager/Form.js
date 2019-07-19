51fe5539f28b0630eb071a19d8a1914fb8258e77
周雅波 <274668775@qq.com>
2019-07-19 10:40:45 +0800

    修复复选框旁侧点击复制数据bug

--- Source/BS/Ayma.Application.Web/Areas/MesDev/Views/OutWorkShopManager/Form.js
+++ Source/BS/Ayma.Application.Web/Areas/MesDev/Views/OutWorkShopManager/Form.js
@@ -218,7 +218,16 @@ $('.am-form-wrap').mCustomScrollbar({theme: "minimal-dark"});
         if (data.length == 0) { //单选
             if (!tmp.get(row)) {
                 tmp.set(row, 1);
-                rows.push(row);
+                var rowFlag = true;
+                //加个循环判断数组重复
+                for (var k = 0; k < rows.length; k++) {
+                    if (rows[k].I_GoodsCode == row.O_GoodsCode && rows[k].I_Batch == row.O_Batch) {
+                        rowFlag = false;
+                    }
+                }
+                if (rowFlag) {
+                    rows.push(row);
+                }
             }
         } else { //多选                  
             for (var i = 0; i < data.length; i++) {
