f91b47876063f6661ab6c626df78f52881859c9f
何鹏 <1363121855@qq.com>
2019-09-26 17:39:23 +0800

    库存查询

---
+++ Source/BS/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.TwoDevelopment/MesDev/I_Period/I_PeriodBLL.cs
@@ -0,0 +1,144 @@
+﻿using Ayma.Util;
+using System;
+using System.Collections.Generic;
+
+namespace Ayma.Application.TwoDevelopment.MesDev
+{
+    /// <summary>
+    /// 创 建：超级管理员
+    /// 日 期：2019-09-26 10:54
+    /// 描 述：保质期查询
+    /// </summary>
+    public partial class I_PeriodBLL : I_PeriodIBLL
+    {
+        private I_PeriodService i_PeriodService = new I_PeriodService();
+
+        #region 获取数据
+
+        /// <summary>
+        /// 获取列表数据
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Mes_InventoryEntity> GetList( string queryJson )
+        {
+            try
+            {
+                return i_PeriodService.GetList(queryJson);
+            }
+            catch (Exception ex)
+            {
+                if (ex is ExceptionEx)
+                {
+                    throw;
+                }
+                else
+                {
+                    throw ExceptionEx.ThrowBusinessException(ex);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 获取列表分页数据
+        /// </summary>
+        /// <param name="pagination">分页参数</param>
+        /// <returns></returns>
+        public IEnumerable<Mes_InventoryEntity> GetPageList(Pagination pagination, string queryJson)
+        {
+            try
+            {
+                return i_PeriodService.GetPageList(pagination, queryJson);
+            }
+            catch (Exception ex)
+            {
+                if (ex is ExceptionEx)
+                {
+                    throw;
+                }
+                else
+                {
+                    throw ExceptionEx.ThrowBusinessException(ex);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 获取实体数据
+        /// </summary>
+        /// <param name="keyValue">主键</param>
+        /// <returns></returns>
+        public Mes_InventoryEntity GetEntity(string keyValue)
+        {
+            try
+            {
+                return i_PeriodService.GetEntity(keyValue);
+            }
+            catch (Exception ex)
+            {
+                if (ex is ExceptionEx)
+                {
+                    throw;
+                }
+                else
+                {
+                    throw ExceptionEx.ThrowBusinessException(ex);
+                }
+            }
+        }
+
+        #endregion
+
+        #region 提交数据
+
+        /// <summary>
+        /// 删除实体数据
+        /// </summary>
+        /// <param name="keyValue">主键</param>
+        /// <returns></returns>
+        public void DeleteEntity(string keyValue)
+        {
+            try
+            {
+                i_PeriodService.DeleteEntity(keyValue);
+            }
+            catch (Exception ex)
+            {
+                if (ex is ExceptionEx)
+                {
+                    throw;
+                }
+                else
+                {
+                    throw ExceptionEx.ThrowBusinessException(ex);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 保存实体数据（新增、修改）
+        /// </summary>
+        /// <param name="keyValue">主键</param>
+        /// <returns></returns>
+        public void SaveEntity(string keyValue, Mes_InventoryEntity entity)
+        {
+            try
+            {
+                i_PeriodService.SaveEntity(keyValue, entity);
+            }
+            catch (Exception ex)
+            {
+                if (ex is ExceptionEx)
+                {
+                    throw;
+                }
+                else
+                {
+                    throw ExceptionEx.ThrowBusinessException(ex);
+                }
+            }
+        }
+
+        #endregion
+
+    }
+}
