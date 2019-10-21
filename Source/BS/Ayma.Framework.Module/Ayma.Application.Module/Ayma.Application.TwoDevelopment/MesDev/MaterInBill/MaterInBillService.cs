b4ec2cd52e0d81332a41e9f1212d792f6a2f4378
周雅波 <274668775@qq.com>
2019-10-21 17:02:20 +0800

    no commit message

--- Source/BS/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.TwoDevelopment/MesDev/MaterInBill/MaterInBillService.cs
+++ Source/BS/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.TwoDevelopment/MesDev/MaterInBill/MaterInBillService.cs
@@ -274,7 +274,7 @@ namespace Ayma.Application.TwoDevelopment.MesDev
                 }
                 if (!queryParam["M_Status"].IsEmpty())
                 {
-                    dp.Add("M_Status", "%" + queryParam["M_Status"].ToString() + "%", DbType.String);
+                    dp.Add("M_Status", "%" + queryParam["M_Status"].ToString() + "%", DbType.Int32);
                     strSql.Append(" AND t.M_Status = @M_Status ");
                 }
                 return this.BaseRepository().FindList<Mes_MaterInHeadEntity>(strSql.ToString(), dp, pagination);
