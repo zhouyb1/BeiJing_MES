ad07a869252b447195afc5f25a58e0ede2a6b1c0
周雅波 <274668775@qq.com>
2019-10-23 10:53:49 +0800

    no commit message

--- Source/BS/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.TwoDevelopment/MesDev/OutWorkShopManager/OutWorkShopManagerService.cs
+++ Source/BS/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.TwoDevelopment/MesDev/OutWorkShopManager/OutWorkShopManagerService.cs
@@ -322,7 +322,7 @@ namespace Ayma.Application.TwoDevelopment.MesDev
                 else
                 {
                     var dp = new DynamicParameters(new { });
-                    dp.Add("@BillType", "成品出库单");
+                    dp.Add("@BillType", "线边仓出库到车间单");
                     dp.Add("@Doucno", "", DbType.String, ParameterDirection.Output);
                     db.ExecuteByProc("sp_GetDoucno", dp);
                     var billNo = dp.Get<string>("@Doucno");//存储过程返回单号
