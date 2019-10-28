024e895b81a25239c0f087da69ba6e8e55881062
何鹏 <1363121855@qq.com>
2019-10-28 13:41:20 +0800

    no commit message

--- Source/BS/Ayma.Application.Web/Areas/MesDev/Views/PickingMater/OrderMaterList.js
+++ Source/BS/Ayma.Application.Web/Areas/MesDev/Views/PickingMater/OrderMaterList.js
@@ -145,7 +145,7 @@ var bootstrap = function ($, ayma) {
                                     }
                                 }
                                var minisChecked = $("[rownum='rownum_girdtable_" + minrowid + "']").find("input[role='checkbox']");
-                               if (!minisChecked.is(":checked")) {
+                                if (!minisChecked.is(":checked")) {
                                     if (row['I_Batch'] > min) {
                                         ayma.alert.error('请优先使用最早批次为' + min + '的【' + row['I_GoodsName'] + '】');
                                     }
