800927a1f713eee509a1033debf82ba227632d6f
周雅波 <274668775@qq.com>
2019-11-06 14:39:03 +0800

    批量操作

--- Source/BS/Ayma.Application.Web/Areas/MesDev/Views/InPrice/Index.js
+++ Source/BS/Ayma.Application.Web/Areas/MesDev/Views/InPrice/Index.js
@@ -61,35 +61,72 @@ var bootstrap = function ($, ayma) {
                     }
                 });
             });
-            // 编辑
-            $('#am_edit').on('click', function () {
-                var keyValue = $('#girdtable').jfGridValue('ID');
-                if (ayma.checkrow(keyValue)) {
-                    ayma.layerForm({
-                        id: 'form',
-                        title: '编辑',
-                        url: top.$.rootUrl + '/MesDev/InPrice/Form?keyValue=' + keyValue,
-                        width: 600,
-                        height: 400,
-                        maxmin: true,
-                        callBack: function (id) {
-                            return top[id].acceptClick(refreshGirdData);
-                        }
-                    });
+            // 保存
+            //$('#am_edit').on('click', function () {
+            //    var keyValue = $('#girdtable').jfGridValue('ID');
+            //    if (ayma.checkrow(keyValue)) {
+            //        ayma.layerForm({
+            //            id: 'form',
+            //            title: '编辑',
+            //            url: top.$.rootUrl + '/MesDev/InPrice/Form?keyValue=' + keyValue,
+            //            width: 600,
+            //            height: 400,
+            //            maxmin: true,
+            //            callBack: function (id) {
+            //                return top[id].acceptClick(refreshGirdData);
+            //            }
+            //        });
+            //    }
+            //});
+
+            $('#am_save').on('click', function() {
+                var arr = [];
+               var rowdatas= $('#girdtable').jfGridGet('rowdata');
+               if (rowdatas==null || rowdatas.length==0) {
+                   ayma.alert.warning('请勾选任意一行！');
+                   return false;
+               }
+               else if (rowdatas.length == undefined) {
+                    arr.push(rowdatas);
+                } else {
+                    arr = rowdatas;
                 }
+                var postData = {
+                    strEntity: JSON.stringify(arr),
+                    strEntity2: JSON.stringify(arr)
+                };
+                $.SaveIndex(top.$.rootUrl + '/MesDev/InPrice/Save', postData, function (res) {
+                    // 保存成功后才回调
+                    if (res.code == 200) {
+                        page.search();
+                    } else {
+                        ayma.alert.error(res.info);
+                        page.search();
+                    }
+                });
             });
             // 删除
-            $('#am_delete').on('click', function () {
-                var keyValue = $('#girdtable').jfGridValue('ID');
-                if (ayma.checkrow(keyValue)) {
-                    ayma.layerConfirm('是否确认删除该项！', function (res) {
-                        if (res) {
-                            ayma.deleteForm(top.$.rootUrl + '/MesDev/InPrice/DeleteForm', { keyValue: keyValue}, function () {
-                                refreshGirdData();
-                            });
-                        }
-                    });
+            $('#am_delete').on('click', function() {
+                var dataList = [];
+                var data = $('#girdtable').jfGridGet('rowdata');
+                if (data == null || data.length == 0) {
+                    ayma.alert.warning('请勾选任意一行！');
+                    return false;
                 }
+                else if (data.length == undefined) {
+                    dataList.push(data);
+                }
+                else {
+                    dataList = data;
+                }
+                ayma.layerConfirm('是否确认删除该项！', function(res) {
+                    if (res) {
+                        ayma.deleteForm(top.$.rootUrl + '/MesDev/InPrice/DeleteForm', { strEntity:  dataList }, function () {
+                            refreshGirdData();
+                        });
+                    }
+                });
+
             });
         },
         // 初始化列表
@@ -101,8 +138,29 @@ var bootstrap = function ($, ayma) {
                     { label: "供应商名称", name: "P_SupplyName", width: 160, align: "left"},
                     { label: "物料编码", name: "P_GoodsCode", width: 160, align: "left"},
                     { label: "物料名称", name: "P_GoodsName", width: 160, align: "left"},
-                    { label: "供应商价格(不含税)", name: "P_InPrice", width: 160, align: "left"},
-                    { label: "税率", name: "P_Itax", width: 160, align: "left" },
+                    {
+                        label: "供应商价格(不含税)", name: "P_InPrice", width: 160, align: "left", editType: 'input', editOp: {
+                            callback: function (rownum, row) {
+                                if (/\D/.test(row.P_InPrice.toString().replace('.', ''))) { //验证只能为数字
+                                    row.P_InPrice = 0;
+                                }
+
+                            }
+                        },formatter: function() {
+                            
+                        }
+                    },
+                    {
+                        label: "税率", name: "P_Itax", width: 160, align: "left", editType: 'input',
+                        editOp: {
+                            callback: function (rownum, row) {
+                                if (/\D/.test(row.P_Itax.toString().replace('.', ''))) { //验证只能为数字
+                                    row.P_Itax = 0;
+                                }
+
+                            }
+                        }
+                    },
                     //{ label: "起始批次", name: "P_StartBatch", width: 160, align: "left" },
                     //{ label: "终止批次", name: "P_EndBatch", width: 160, align: "left" },
                     //{ label: "添加人", name: "P_CreateBy", width: 160, align: "left"},
@@ -110,7 +168,8 @@ var bootstrap = function ($, ayma) {
                 ],
                 mainId:'ID',
                 reloadSelected: true,
-                isPage: true
+                isPage: true,
+                isMultiselect:true
             });
         },
         search: function (param) {
@@ -123,5 +182,7 @@ var bootstrap = function ($, ayma) {
     refreshGirdData = function () {
         page.search();
     };
+
+
     page.init();
 }
