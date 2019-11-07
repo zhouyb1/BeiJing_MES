dbb34ed7b59430f9bfae1a4ea80d7a3d175215d9
何鹏 <1363121855@qq.com>
2019-11-07 10:39:36 +0800

    no commit message

---
+++ Source/BS/Ayma.Application.Web/Areas/MesDev/Controllers/CustomController.cs
@@ -0,0 +1,120 @@
+﻿using Ayma.Application.TwoDevelopment.MesDev;
+using Ayma.Util;
+using System.Web.Mvc;
+
+namespace Ayma.Application.Web.Areas.MesDev.Controllers
+{
+    /// <summary>
+    /// 创 建：超级管理员
+    /// 日 期：2019-11-06 16:30
+    /// 描 述：客户表
+    /// </summary>
+    public partial class CustomController : MvcControllerBase
+    {
+        private CustomIBLL customIBLL = new CustomBLL();
+
+        #region 视图功能
+
+        /// <summary>
+        /// 主页面
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Index()
+        {
+             return View();
+        }
+        /// <summary>
+        /// 表单页
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Form()
+        {
+             return View();
+        }
+        #endregion
+
+        #region 获取数据
+
+        /// <summary>
+        /// 获取列表数据
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [AjaxOnly]
+        public ActionResult GetList( string queryJson )
+        {
+            var data = customIBLL.GetList(queryJson);
+            return Success(data);
+        }
+        /// <summary>
+        /// 获取列表分页数据
+        /// </summary>
+        /// <param name="pagination">分页参数</param>
+        /// <returns></returns>
+        [HttpGet]
+        [AjaxOnly]
+        public ActionResult GetPageList(string pagination, string queryJson)
+        {
+            Pagination paginationobj = pagination.ToObject<Pagination>();
+            var data = customIBLL.GetPageList(paginationobj, queryJson);
+            var jsonData = new
+            {
+                rows = data,
+                total = paginationobj.total,
+                page = paginationobj.page,
+                records = paginationobj.records
+            };
+            return Success(jsonData);
+        }
+        /// <summary>
+        /// 获取表单数据
+        /// </summary>
+        /// <param name="keyValue">主键</param>
+        /// <returns></returns>
+        [HttpGet]
+        [AjaxOnly]
+        public ActionResult GetFormData(string keyValue)
+        {
+   
+            var data = customIBLL.GetEntity(keyValue);
+            var jsonData = new
+            {
+                data = data,
+            };
+            return Success(jsonData);
+        }
+        #endregion
+
+        #region 提交数据
+
+        /// <summary>
+        /// 删除实体数据
+        /// </summary>
+        /// <param name="keyValue">主键</param>
+        /// <returns></returns>
+        [HttpPost]
+        [AjaxOnly]
+        public ActionResult DeleteForm(string keyValue)
+        {
+            customIBLL.DeleteEntity(keyValue);
+            return Success("删除成功！");
+        }
+        /// <summary>
+        /// 保存实体数据（新增、修改）
+        /// </summary>
+        /// <param name="keyValue">主键</param>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AjaxOnly]
+        public ActionResult SaveForm(string keyValue,Mes_CustomerEntity entity)
+        {
+            customIBLL.SaveEntity(keyValue, entity);
+            return Success("保存成功！");
+        }
+        #endregion
+
+    }
+}
