4b7ca7a08456d343fe1f6aca463b838bfb608aa0
何鹏 <1363121855@qq.com>
2019-11-08 10:42:00 +0800

    no commit message

---
+++ Source/BS/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.Mapping/MesDev/Mes_OtherInDetailMap.cs
@@ -0,0 +1,27 @@
+﻿using Ayma.Application.TwoDevelopment.MesDev;
+using System.Data.Entity.ModelConfiguration;
+
+namespace  Ayma.Application.Mapping
+{
+    /// <summary>
+    /// 创 建：超级管理员
+    /// 日 期：2019-11-07 17:56
+    /// 描 述：其它入库单据从表
+    /// </summary>
+    public partial class Mes_OtherInDetailMap : EntityTypeConfiguration<Mes_OtherInDetailEntity>
+    {
+        public Mes_OtherInDetailMap()
+        {
+            #region 表、主键
+            //表
+            this.ToTable("MES_OTHERINDETAIL");
+            //主键
+            this.HasKey(t => t.ID);
+            #endregion
+
+            #region 配置关系
+            #endregion
+        }
+    }
+}
+
---
+++ Source/BS/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.Mapping/MesDev/Mes_OtherInHeadMap.cs
@@ -0,0 +1,27 @@
+﻿using Ayma.Application.TwoDevelopment.MesDev;
+using System.Data.Entity.ModelConfiguration;
+
+namespace  Ayma.Application.Mapping
+{
+    /// <summary>
+    /// 创 建：超级管理员
+    /// 日 期：2019-11-07 13:51
+    /// 描 述：其它入库单
+    /// </summary>
+    public partial class Mes_OtherInHeadMap : EntityTypeConfiguration<Mes_OtherInHeadEntity>
+    {
+        public Mes_OtherInHeadMap()
+        {
+            #region 表、主键
+            //表
+            this.ToTable("MES_OTHERINHEAD");
+            //主键
+            this.HasKey(t => t.ID);
+            #endregion
+
+            #region 配置关系
+            #endregion
+        }
+    }
+}
+
