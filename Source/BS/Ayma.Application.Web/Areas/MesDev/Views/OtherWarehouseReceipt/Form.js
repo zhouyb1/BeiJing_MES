0baf46e763c8997b1b28b025ba8f159f41a23f05
何鹏 <1363121855@qq.com>
2019-11-08 10:42:59 +0800

    no commit message

---
+++ Source/BS/Ayma.Application.Web/Areas/MesDev/Views/OtherWarehouseReceipt/Form.js
@@ -0,0 +1,235 @@
+﻿/* * 创建人：超级管理员
+ * 日  期：2019-11-07 13:51
+ * 描  述：其它入库单
+ */
+var refreshGirdData;//表格商品添加
+var RemoveGridData;//移除表格
+var acceptClick;
+var keyValue = request('keyValue');
+var tmp = new Map();
+var keyValue = request('keyValue');
+var parentFormId = request('formId');//上一级formId
+var status = request('status');
+var bootstrap = function ($, ayma) {
+    "use strict";
+    var selectedRow = ayma.frameTab.currentIframe().selectedRow;
+    var page = {
+        init: function () {
+            $('.am-form-wrap').mCustomScrollbar({ theme: "minimal-dark" });
+            page.bind();
+            page.initData();
+        },
+        bind: function () {
+            //绑定仓库
+            var dfop = {
+                type: 'default',
+                value: 'S_Name',
+                text: 'S_Name',
+                // 展开最大高度
+                maxHeight: 200,
+                // 是否允许搜索
+                allowSearch: true,
+                // 访问数据接口地址
+                url: top.$.rootUrl + '/MesDev/Tools/GetOriginalStockList',
+                // 访问数据接口参数
+                param: {}
+            };
+            //绑定仓库
+            $('#O_StockName').select(dfop).on('change', function () {
+                if (status == 1) {
+                    $('#Mes_OtherInDetail').jfGridSet('refreshdata', { rowdatas: [] });
+                }
+                var code = $(this).selectGet();
+                $.ajax({
+                    type: "get",
+                    url: top.$.rootUrl + '/MesDev/Tools/ByCodeGetStockEntity',
+                    data: { code: code },
+                    success: function (data) {
+                        var entity = JSON.parse(data).data;
+                        $("#O_StockCode").val(entity.S_Code);
+                    }
+                });
+            });
+            //添加商品
+            $("#am_add").on("click", function () {
+                if ($("#O_StockCode").val() == "") {
+                    ayma.alert.error("请选择仓库！");
+                    return false;
+                }
+                var code = $('#M_SupplyCode').selectGet();
+                    ayma.layerForm({
+                        id: 'GoodsListIndexForm',
+                        title: '添加物料',
+                        url: top.$.rootUrl + '/MesDev/OtherWarehouseReceipt/GoodsListIndex?formId=' + parentFormId + '&O_StockCode=' + $("#O_StockCode").val(),
+                        width: 750,
+                        height: 600,
+                        maxmin: true,
+                        callBack: function (id, index) {
+                            return top[id].closeWindow();
+                        }
+                    });
+                    return true;             
+            });
+            //单据状态
+            $("#O_Status").DataItemSelect({ code: 'MaterInStatus' });
+            $('#Mes_OtherInDetail').jfGrid({
+                headData: [
+                     {
+                         label: 'ID', name: 'ID', width: 160, align: 'left', editType: 'label', hidden: true
+                     },
+
+                    {
+                        label: '物料编码', name: 'O_GoodsCode', width: 140, align: 'left', editType: 'label'
+                    },
+                    {
+                        label: '物料名称', name: 'O_GoodsName', width: 140, align: 'left', editType: 'label'
+                    },
+                    {
+                        label: '单位', name: 'O_Unit', width: 80, align: 'left', editType: 'label'
+                    },
+                     {
+                         label: '数量', name: 'O_Qty', width: 80, align: 'left', editType: 'input',
+                         editOp: {
+                             callback: function (rownum, row) {
+                                 if (/\D/.test(row.O_Qty.toString().replace('.', ''))) { //验证只能为数字
+                                     row.O_Qty = 0;
+                                 }
+
+                             }
+                         }
+                     },
+                     {
+                         label: '价格', name: 'O_Price', width: 80, align: 'left', editType: 'label',
+                         editOp: {
+                             callback: function (rownum, row) {
+                                 if (/\D/.test(row.O_Price.toString().replace('.', ''))) { //验证只能为数字
+                                     row.O_Price = 0;
+                                 }
+
+                             }
+                         }
+                     },//最新维护的价格
+                    {
+                        label: '批次', name: 'O_Batch', width: 100, align: 'left', editType: 'input',
+                        editOp: {
+                            callback: function (rownum, row) {
+                                if (/\D/.test(row.O_Batch.toString().replace('.', ''))) { //验证只能为数字
+                                    row.O_Batch = 0;
+                                }
+
+                            }
+                        }
+                    },
+                    {
+                        label: '备注', name: 'O_Remark', width: 160, align: 'left', editType: 'input'
+                    },
+                ],
+                isAutoHeight: false,
+                footerrow: true,
+                minheight: 320,
+                isEidt: true,
+                isMultiselect: true,
+                height: 300,
+                inputCount: 4
+            });
+        },
+        initData: function () {
+            if (!!keyValue) {
+                $.SetForm(top.$.rootUrl + '/MesDev/OtherWarehouseReceipt/GetFormData?keyValue=' + keyValue, function (data) {
+                    for (var id in data) {
+                        if (!!data[id].length && data[id].length > 0) {
+                            $('#Mes_OtherInDetail').jfGridSet('refreshdata', { rowdatas: data[id] });
+                        }
+                        else {
+                            $('[data-table="' + id + '"]').SetFormData(data[id]);
+                        }
+                    }
+                });
+            } else {
+                $("#O_Status").selectSet(1);//新增时默认为单据生成
+            }
+        },
+        search: function (data) {
+            data = data || {};
+            $('#Mes_OtherInDetail').jfGridSet('refreshdata', { rowdatas: data });
+        }
+    };
+    // 保存数据
+    acceptClick = function (callBack) {
+        if (!$('body').Validform()) {
+            return false;
+        }
+        var data = $('#Mes_OtherInDetail').jfGridGet('rowdatas');
+        if (data.length == 0 || data[0].O_GoodsCode == null) {
+            ayma.alert.error("请添加物料！");
+            return false;
+        }
+        var postData = {};
+        var obj = $('[data-table="Mes_OtherInHead"]').GetFormData();
+        postData.strEntity = JSON.stringify(obj);
+        postData.strmes_MaterOtherInDetailList = JSON.stringify($('#Mes_OtherInDetail').jfGridGet('rowdatas'));
+        $.SaveForm(top.$.rootUrl + '/MesDev/OtherWarehouseReceipt/SaveForm?keyValue=' + keyValue, postData, function (res) {
+            // 保存成功后才回调
+            if (!!callBack) {
+                callBack();
+            }
+        });
+    };
+    //表格商品添加
+    refreshGirdData = function (data, row) {
+        var rows = $('#Mes_OtherInDetail').jfGridGet('rowdatas');
+        if (data.length == 0) { //单选
+            if (!tmp.get(row)) {
+                tmp.set(row, 1);
+                var flagRow = true;
+                //加个循环判断数组重复
+                for (var k = 0; k < rows.length; k++) {
+                    if (rows[k].O_GoodsCode == row.G_Code) {
+                        flagRow = false;
+                    }
+                }
+                if (flagRow) {
+                    rows.push(row);
+                }
+            }
+        } else { //多选                  
+            for (var i = 0; i < data.length; i++) {
+                if (!tmp.get(data[i])) {
+                    tmp.set(data[i], 1);
+                    var flag = true;
+                    //加个循环判断数组重复
+                    for (var j = 0; j < rows.length; j++) {
+                        if (rows[j].O_GoodsCode == data[i].G_Code) {
+                            flag = false;
+                        }
+                    }
+                    if (flag) {
+                        rows.push(data[i]);
+                    }
+                }
+            }
+        }
+
+        //数组过滤
+        var filterarray = $.grep(rows, function (item) {
+            return item["O_GoodsCode"] != undefined;
+        });
+        page.search(filterarray);
+    };
+    //表格商品删除
+    RemoveGridData = function (row) {
+        var rows = $('#Mes_OtherInDetail').jfGridGet('rowdatas');
+
+        for (var i = 0; i < rows.length; i++) {
+            if (rows[i]["O_GoodsCode"] == row["G_Code"]) {
+                rows.splice(i, 1);
+                tmp.delete(row);
+                page.search(rows);
+            }
+        }
+    };
+    top.NewGirdData = function () {
+        return $('#Mes_OtherInDetail').jfGridGet('rowdatas');
+    }
+    page.init();
+}
---
+++ Source/BS/Ayma.Application.Web/Areas/MesDev/Views/OtherWarehouseReceipt/Index.js
@@ -0,0 +1,240 @@
+﻿/* * 创建人：超级管理员
+ * 日  期：2019-11-07 13:51
+ * 描  述：其它入库单
+ */
+var selectedRow;
+var refreshGirdData;
+var startTime;
+var endTime;
+var bootstrap = function ($, ayma) {
+    "use strict";
+    var page = {
+        init: function () {
+            page.initGird();
+            page.bind();
+        },
+        bind: function () {
+                  // 时间搜索框
+            $('#datesearch').amdate({
+                dfdata: [
+                    { name: '今天', begin: function () { return ayma.getDate('yyyy-MM-dd 00:00:00') }, end: function () { return ayma.getDate('yyyy-MM-dd 23:59:59') } },
+                    { name: '近7天', begin: function () { return ayma.getDate('yyyy-MM-dd 00:00:00', 'd', -6) }, end: function () { return ayma.getDate('yyyy-MM-dd 23:59:59') } },
+                    { name: '近1个月', begin: function () { return ayma.getDate('yyyy-MM-dd 00:00:00', 'm', -1) }, end: function () { return ayma.getDate('yyyy-MM-dd 23:59:59') } },
+                    { name: '近3个月', begin: function () { return ayma.getDate('yyyy-MM-dd 00:00:00', 'm', -3) }, end: function () { return ayma.getDate('yyyy-MM-dd 23:59:59') } }
+                ],
+                // 月
+                mShow: false,
+                premShow: false,
+                // 季度
+                jShow: false,
+                prejShow: false,
+                // 年
+                ysShow: false,
+                yxShow: false,
+                preyShow: false,
+                yShow: false,
+                // 默认
+                dfvalue: '1',
+                selectfn: function (begin, end) {
+                    startTime = begin;
+                    endTime = end;
+                    page.search();
+                }
+            });
+            $('#multiple_condition_query').MultipleQuery(function (queryJson) {
+                page.search(queryJson);
+            }, 180, 500);
+            $('#O_Status').DataItemSelect({ code: 'MaterInStatus' });
+            // 刷新
+            $('#am_refresh').on('click', function () {
+                location.reload();
+            });
+
+            $('#O_StockName').select({
+                type: 'default',
+                value: 'S_Code',
+                text: 'S_Name',
+                // 展开最大高度
+                maxHeight: 200,
+                // 是否允许搜索
+                allowSearch: true,
+                // 访问数据接口地址
+                url: top.$.rootUrl + '/MesDev/Tools/GetOriginalStockList',
+                // 访问数据接口参数
+                param: {}
+
+            });
+            // 新增
+            $('#am_add').on('click', function () {
+                selectedRow = null;
+                ayma.layerForm({
+                    id: 'form',
+                    title: '新增',
+                    url: top.$.rootUrl + '/MesDev/OtherWarehouseReceipt/Form?formId=form',
+                    width: 900,
+                    height: 600,
+                    maxmin: true,
+                    callBack: function (id) {
+                        return top[id].acceptClick(refreshGirdData);
+                    }
+                });
+            });
+            // 编辑
+            $('#am_edit').on('click', function () {
+                var keyValue = $('#girdtable').jfGridValue('ID');
+                var statu = $('#girdtable').jfGridValue('O_Status');
+                selectedRow = $('#girdtable').jfGridGet('rowdata');
+                if (ayma.checkrow(keyValue)) {
+                    ayma.layerForm({
+                        id: 'form',
+                        title: '编辑',
+                        url: top.$.rootUrl + '/MesDev/OtherWarehouseReceipt/Form?keyValue=' + keyValue + '&status=' + statu + '&formId=form',
+                        width: 900,
+                        height: 600,
+                        maxmin: true,
+                        callBack: function (id) {
+                            return top[id].acceptClick(refreshGirdData);
+                        }
+                    });
+                }
+            });
+            // 双击编辑
+            $('#girdtable').on('dblclick', function () {
+                var keyValue = $('#girdtable').jfGridValue('ID');
+                var statu = $('#girdtable').jfGridValue('O_Status');
+                selectedRow = $('#girdtable').jfGridGet('rowdata');
+                if (ayma.checkrow(keyValue)) {
+                    ayma.layerForm({
+                        id: 'form',
+                        title: '编辑',
+                        url: top.$.rootUrl + '/MesDev/OtherWarehouseReceipt/Form?keyValue=' + keyValue + '&status=' + statu,
+                        width: 900,
+                        height: 600,
+                        maxmin: true,
+                        callBack: function (id) {
+                            return top[id].acceptClick(refreshGirdData);
+                        }
+                    });
+                }
+            });
+            // 快速打印
+            $('#am_print').on('click', function () {
+                var keyValue = $('#girdtable').jfGridValue('O_OtherInNo');
+                if (ayma.checkrow(keyValue, true)) {
+                    ayma.layerForm({
+                        id: 'SaleOutReport',
+                        title: '其它入库单打印',
+                        url: top.$.rootUrl + '/MesDev/OtherWarehouseReceipt/PrintReport?keyValue=' + keyValue + "&report=OtherWarehouseReport&data=OtherWarehouse",
+                        width: 1000,
+                        height: 800,
+                        maxmin: true,
+                        callBack: function (id) {
+                            return top[id].acceptClick(refreshGirdData);
+                        }
+                    });
+                }
+            });
+            //审核单据
+            $("#am_auditing").on('click', function () {
+                var keyValue = $("#girdtable").jfGridValue("ID");
+                if (ayma.checkrow(keyValue)) {
+                    var status = $("#girdtable").jfGridValue("O_Status");
+                    if (status != "1") {
+                        ayma.alert.error("已审核");
+                        return false;
+                    }
+                    ayma.layerConfirm('是否确认审核该单据！', function (res) {
+                        if (res) {
+                            ayma.postForm(top.$.rootUrl + '/MesDev/Tools/AuditingBill', { keyValue: keyValue, tables: 'Mes_OtherInHead', field: 'O_Status' }, function () {
+                                refreshGirdData();
+                            });
+                        }
+                    });
+                }
+            });
+            //// 删除
+            //$('#am_delete').on('click', function () {
+            //    var keyValue = $('#girdtable').jfGridValue('ID');
+            //    if (ayma.checkrow(keyValue)) {
+            //        ayma.layerConfirm('是否确认删除该项！', function (res) {
+            //            if (res) {
+            //                ayma.deleteForm(top.$.rootUrl + '/MesDev/OtherWarehouseReceipt/DeleteForm', { keyValue: keyValue}, function () {
+            //                });
+            //            }
+            //        });
+            //    }
+            //});
+            ////删除单据
+            //$("#am_delete").on('click', function () {
+            //    var orderNo = $("#girdtable").jfGridValue("O_OtherInNo");
+            //    if (ayma.checkrow(orderNo)) {
+            //        var status = $("#girdtable").jfGridValue("O_Status");
+            //        if (status == "2") {
+            //            ayma.alert.error("已审核不能删除");
+            //            return false;
+            //        }
+            //        ayma.layerConfirm('是否确认删除该单据！', function (res) {
+            //            if (res) {
+            //                ayma.postForm(top.$.rootUrl + '/MesDev/Tools/PostOrCancelOrDeleteMaterInBill', { orderNo: orderNo, proc: 'sp_MaterIn_Delete', type: 3 }, function () {
+            //                    refreshGirdData();
+            //                });
+            //            }
+            //        });
+            //    }
+            //});
+        },
+        initGird: function () {
+            $('#girdtable').AuthorizeJfGrid({
+                url: top.$.rootUrl + '/MesDev/OtherWarehouseReceipt/GetPageList',
+                headData: [
+                                {
+                                    label: "单据状态", name: "O_Status", width: 90, align: "left",
+                                    formatterAsync: function (callback, value, row) {
+                                        ayma.clientdata.getAsync('dataItem', {
+                                            key: value,
+                                            code: 'MaterInStatus',
+                                            callback: function (_data) {
+
+                                                if (value == 1) {
+                                                    callback("<span class='label label-default'>" + _data.text + "</span>");
+                                                } else if (value == 2) {
+                                                    callback("<span class='label label-info'>" + _data.text + "</span>");
+                                                } else if (value == 3) {
+                                                    callback("<span class='label label-success'>" + _data.text + "</span>");
+                                                } else {
+                                                    callback("<span class='label label-danger'>" + _data.text + "</span>");
+                                                }
+                                            }
+                                        });
+                                    }
+                                },
+                        { label: '单号', name: 'O_OtherInNo', width: 200, align: "left" },
+                        { label: '仓库编码', name: 'O_StockCode', width: 150, align: "left" },
+                        { label: '仓库名称', name: 'O_StockName', width: 150, align: "left" },
+                        { label: '添加人', name: 'O_CreateBy', width: 150, align: "left" },
+                        { label: '创建时间', name: 'O_CreateDate', width: 150, align: "left" },
+                        { label: '修改人', name: 'O_UpdateBy', width: 150, align: "left" },
+                        { label: '修改时间', name: 'O_UpdateDate', width: 150, align: "left" },
+                        { label: '备注', name: 'O_Remark', width: 150, align: "left" },
+                        { label: '月结', name: 'MonthBalance', width: 150, align: "left" },
+                ],
+                mainId:'ID',
+                reloadSelected: true,
+                isPage: true,
+                sidx: "O_CreateDate",
+                sord: "DESC"
+            });
+            page.search();
+        },
+        search: function (param) {
+            param = param || {};
+            param.StartTime = startTime;
+            param.EndTime = endTime;
+            $('#girdtable').jfGridSet('reload', { param: { queryJson: JSON.stringify(param) } });
+        }
+    };
+    refreshGirdData = function () {
+        page.search();
+    };
+    page.init();
+}
