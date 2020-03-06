ec9aa8182c8e81bedb795b8bdce6d6aeddc61e87
周雅波 <274668775@qq.com>
2020-03-06 10:04:34 +0800

    no commit message

--- Source/BS/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.TwoDevelopment/MesDev/RequistBill/RequistBillService.cs
+++ Source/BS/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.TwoDevelopment/MesDev/RequistBill/RequistBillService.cs
@@ -283,8 +283,8 @@ namespace Ayma.Application.TwoDevelopment.MesDev
                                       ,d.R_Qty
                                       ,d.R_Batch
                                       ,d.R_Remark
-                                      ,dbo.GetPrice(d.R_GoodsCode,DATEPART(mm,h.R_UploadDate)) R_Price
-                                      ,dbo.GetPrice(d.R_GoodsCode,DATEPART(mm,h.R_UploadDate))* d.R_Qty R_Amount
+                                      ,dbo.GetPrice(d.R_GoodsCode,CONVERT(VARCHAR(6),h.R_UploadDate,112)) R_Price
+                                      ,dbo.GetPrice(d.R_GoodsCode,CONVERT(VARCHAR(6),h.R_UploadDate,112))* d.R_Qty R_Amount
                                   FROM dbo.Mes_RequistHead h INNER JOIN dbo.Mes_RequistDetail d ON h.R_RequistNo =d.R_RequistNo where h.R_RequistNo =@R_RequistNo");
                 var dp = new DynamicParameters(new {});
                 dp.Add("R_RequistNo",keyValue,DbType.String);
