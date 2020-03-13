e93a1f14f354bab8ce7513819fb2685c0561eade
廖水法 <1017508235@qq.com>
2020-03-13 14:44:43 +0800

    no commit message

---
+++ Source/BS/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.TwoDevelopment/MesDev/MonthBalance/Extend/Mes_Product.cs
@@ -0,0 +1,40 @@
+﻿namespace Ayma.Application.TwoDevelopment.MesDev
+{
+    /// <summary>
+    /// 配方
+    /// </summary>
+    public class Mes_Product
+    {
+        /// <summary>
+        /// 配方ID
+        /// </summary>
+        public string M_ID { get; set; }
+
+        /// <summary>
+        /// 父级ID
+        /// </summary>
+        public string M_ParentID { get; set; }
+
+        /// <summary>
+        /// 物料编码
+        /// </summary>
+        public string M_GoodsCode { get; set; }
+
+        /// <summary>
+        /// 物料名称
+        /// </summary>
+        public string M_GoodsName { get; set; }
+
+
+        /// <summary>
+        /// 物料类型
+        /// </summary>
+        public int M_Kind { get; set; }
+
+        /// <summary>
+        /// 物料级别
+        /// </summary>
+        public int M_Level { get; set; }
+
+    }
+}
--- Source/BS/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.TwoDevelopment/MesDev/MonthBalance/MonthBalanceService.cs
+++ Source/BS/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.TwoDevelopment/MesDev/MonthBalance/MonthBalanceService.cs
@@ -1163,8 +1163,6 @@ SELECT
                     db.Commit();
                 }
                 #endregion
-
-
                 return success;
             }
 
