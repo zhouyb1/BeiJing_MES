81158316b5a814e3231f86e8168c977839bd691a
何鹏 <1363121855@qq.com>
2020-03-17 15:10:18 +0800

    no commit message

--- Source/BS/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.TwoDevelopment/MesDev/MaterialsSum/MaterialsSumService.cs
+++ Source/BS/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.TwoDevelopment/MesDev/MaterialsSum/MaterialsSumService.cs
@@ -681,7 +681,7 @@ namespace Ayma.Application.TwoDevelopment.MesDev
                 dp.Add("StartTime", StartTime, DbType.DateTime);
                 dp.Add("EndTime", EndTime, DbType.DateTime);
                 dp.Add("Time", Time, DbType.DateTime);
-                strSql.Append(@"                            select* from  ( select 
+                strSql.Append(@"     select* from  ( select 鸡就是鸡
                                     (select S_Name from Mes_Stock where S_Code=s.G_StockCode)as S_Name,
                                      s.G_StockCode
 								    ,s.G_Code 
@@ -690,7 +690,7 @@ namespace Ayma.Application.TwoDevelopment.MesDev
                                     ,@StartTime as 'startTime'
                                     ,@EndTime as 'endTime'
                                     ,(select ISNULL(O_SalePrice,0) from Mes_OutPrice where O_GoodsCode=s.G_Code ) as outPrice
-                                    ,(select ISNULL(O_SalePrice,0) from Mes_OutPrice where O_GoodsCode=s.G_Code )*(select  ISNULL(SUM(S_Qty),0) from Mes_SaleDetail where S_GoodsCode=s.G_Code  and S_SaleNo in(select S_SaleNo from Mes_SaleHead where (S_CreateDate >=@StartTime and S_CreateDate <=@EndTime)and S_Status=3)) as outamount
+                                    ,(select ISNULL(O_SalePrice,0) from Mes_OutPrice where O_Good   sCode=s.G_Code )*(select  ISNULL(SUM(S_Qty),0) from Mes_SaleDetail where S_GoodsCode=s.G_Code  and S_SaleNo in(select S_SaleNo from Mes_SaleHead where (S_CreateDate >=@StartTime and S_CreateDate <=@EndTime)and S_Status=3)) as outamount
 			                	   	,(select  ISNULL(SUM(B_Qty),0) from Mes_BackStockDetail where B_GoodsCode=s.G_Code  and B_BackStockNo in(select B_BackStockNo from Mes_BackStockHead where (B_CreateDate >=@StartTime and B_CreateDate <=@EndTime)and B_Status=3))as withdrawingnumber
 									,(select  ISNULL(SUM(S_Qty),0) from Mes_SaleDetail where S_GoodsCode=s.G_Code  and S_SaleNo in(select S_SaleNo from Mes_SaleHead where (S_CreateDate >=@StartTime and S_CreateDate <=@EndTime)and S_Status=3)) as materialssales 
 									,(select ISNULL(SUM(S_Qty),0) from Mes_ScrapDetail where S_GoodsCode=s.G_Code  and S_ScrapNo in(select S_ScrapNo from Mes_ScrapHead where (S_CreateDate >=@StartTime and S_CreateDate <=@EndTime)and S_Status=3)) as scrapist  
