a3f7e7183db5128d15196e1a703517b9e3493215
何鹏 <1363121855@qq.com>
2020-05-20 11:43:22 +0800

    no commit message

--- Source/BS/Ayma.Application.Web/Areas/MesDev/Views/MaterInBill/Index.js
+++ Source/BS/Ayma.Application.Web/Areas/MesDev/Views/MaterInBill/Index.js
@@ -225,9 +225,6 @@ var bootstrap = function ($, ayma) {
                     },
                     { label: "单据编号", name: "M_MaterInNo", width: 130, align: "center", sort: true },
                     { label: "供应商名称", name: "M_SupplyName", width: 140, align: "center" },
-                    { label: "供应商编码", name: "M_SupplyCode", width: 90, align: "center", hidden: true },
-                    { label: "仓库编码", name: "M_StockCode", width: 90, align: "center" ,hidden:true},
-                    { label: "仓库名称", name: "M_StockName", width: 160, align: "center", hidden: true },
                     { label: "添加人", name: "M_CreateBy", width: 90, align: "center" },
                     {
                         label: "单据时间", name: "M_OrderDate", width: 100, align: "center", sort: true ,
