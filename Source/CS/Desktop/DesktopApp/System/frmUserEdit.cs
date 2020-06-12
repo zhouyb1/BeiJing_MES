ffdd9e268c0f91eff6ae7bf69e385c308821e977
svit <svit.gui@ayma.com.cn>
2020-06-12 10:20:25 +0800

    no commit message

--- Source/CS/Desktop/DesktopApp/System/frmUserEdit.cs
+++ Source/CS/Desktop/DesktopApp/System/frmUserEdit.cs
@@ -249,31 +249,37 @@ namespace DesktopApp
 
         private void Down(string strPath,string url)
         {
-            
-            // 设置参数
-            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
-
-            //发送请求并获取相应回应数据
-            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
-            //直到request.GetResponse()程序才开始向目标网页发送Post请求
-            Stream responseStream = response.GetResponseStream();
-
-       
-            //创建本地文件写入流
-            Stream stream = new FileStream(strPath, FileMode.Create);
-
-            byte[] bArr = new byte[1024];
-            int size = responseStream.Read(bArr, 0, (int)bArr.Length);
-            while (size > 0)
+            try
             {
-                stream.Write(bArr, 0, size);
-                size = responseStream.Read(bArr, 0, (int)bArr.Length);
+                // 设置参数
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+
+                //发送请求并获取相应回应数据
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                //直到request.GetResponse()程序才开始向目标网页发送Post请求
+                Stream responseStream = response.GetResponseStream();
+
+
+                //创建本地文件写入流
+                Stream stream = new FileStream(strPath, FileMode.Create);
+
+                byte[] bArr = new byte[1024];
+                int size = responseStream.Read(bArr, 0, (int)bArr.Length);
+                while (size > 0)
+                {
+                    stream.Write(bArr, 0, size);
+                    size = responseStream.Read(bArr, 0, (int)bArr.Length);
+                }
+                stream.Close();
+                responseStream.Close();
+                //return path;
+
+
             }
-            stream.Close();
-            responseStream.Close();
-            //return path;
-
-
+            catch(Exception ex)
+            {
+                ;
+            }
 
 
 
